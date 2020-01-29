1168f5949ba23ad659085d1cd309444bdeb7057a
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk.com>
2020-01-29 15:59:28 +0000

    Adding Spring Boot References

---
+++ api/src/main/java/gov/uk/courtdata/MaatCourtDataApplication.java
@@ -0,0 +1,14 @@
+package gov.uk.courtdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtdataApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourtdataApplication.class, args);
+	}
+
+}

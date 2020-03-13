4a24f6e3a808d00ea2783b96da4c4f3ab0e14186
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk.com>
2020-03-13 14:58:01 +0000

    Fixing LAA Status Lin Register Update Query Issue

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/WqLinkRegisterRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/WqLinkRegisterRepository.java
@@ -20,7 +20,4 @@ public interface WqLinkRegisterRepository extends JpaRepository<WqLinkRegisterEn
     @Query(value = "SELECT count(*)  FROM MLA.XXMLA_WQ_LINK_REGISTER WHERE maat_id  = ?1 and removed_tx_id  is null ", nativeQuery = true)
     Integer getCountByMaatId(final Integer maatId);
 
-
-    @Query(value = "UPDATE MLA.XXMLA_WQ_LINK_REGISTER link SET link.mlr_cat = ?1 where link.CASE_ID = ?2", nativeQuery = true)
-    void updateCategory(Integer category,Integer caseId);
 }

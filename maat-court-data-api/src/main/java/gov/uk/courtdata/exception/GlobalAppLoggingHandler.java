9a470a5a22b41786873c1f05551d80ed3de8e037
atharmajeed <athar@tyrocircle.com>
2020-07-14 11:06:22 +0100

    Code review changes.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/exception/GlobalAppLoggingHandler.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/exception/GlobalAppLoggingHandler.java
@@ -2,8 +2,6 @@ package gov.uk.courtdata.exception;
 
 import com.google.gson.Gson;
 import gov.uk.courtdata.model.LaaTransactionLogging;
-import io.sentry.Sentry;
-import io.sentry.event.UserBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -20,13 +18,12 @@ public class GlobalAppLoggingHandler {
     public static final String MESSAGE = "message";
     public static final String MAATID = "maatId";
 
+    private Gson gson;
+
 
     /**
      * This method will execute whenever a exception occour in any of the following (service) package and a class has method name receive.
      * If we are adding new queue listener then we should follow the same existing pattern.
-     *
-     * @param joinPoint
-     * @param ex
      */
     @AfterThrowing(pointcut = "execution(* gov.uk.courtdata.*.service.*.receive(..))", throwing = "ex")
     public void afterThrowingHearingDetail(JoinPoint joinPoint, RuntimeException ex) {
@@ -37,8 +34,6 @@ public class GlobalAppLoggingHandler {
 
     /**
      * This method will be called at the end when there is a successful message processing.
-     *
-     * @param joinPoint
      */
     @AfterReturning(" execution(* gov.uk.courtdata.*.service.*.receive(..))  ")
     public void afterProcess(JoinPoint joinPoint) {
@@ -47,8 +42,6 @@ public class GlobalAppLoggingHandler {
 
     /**
      * This method will called every time at the of queue consumer, regardless of the outcome.
-     *
-     * @param joinPoint
      */
     @After(" execution(* gov.uk.courtdata.*.service.*.receive(..))  ")
     public void afterProcessEnds(JoinPoint joinPoint) {
@@ -60,21 +53,11 @@ public class GlobalAppLoggingHandler {
      * This method will be called automatically for all the queue (jms) listeners. More specifically classes within
      * service package where method name is receive. For any new queue listener we should follow the same naming convention.
      *
-     * @param joinPoint
-     * @param message
      */
     @Before(" execution(* gov.uk.courtdata.*.service.*.receive(..))  && args(message,..) ")
     public void beforeQueueMessageRec(JoinPoint joinPoint, String message) {
 
-        Gson gson = new Gson();
         LaaTransactionLogging laaTransactionLogging = gson.fromJson(message, LaaTransactionLogging.class);
-
-        Sentry.getContext().addTag(LAA_TRANSACTION_ID,
-                laaTransactionLogging.getLaaTransactionId() != null ? laaTransactionLogging.getLaaTransactionId().toString() : "");
-        Sentry.getContext().addTag(CASE_URN,
-                laaTransactionLogging.getCaseUrn() != null ? laaTransactionLogging.getCaseUrn() : "");
-        Sentry.getContext().setUser(new UserBuilder()
-                .setId(laaTransactionLogging.getMaatId() != null ? laaTransactionLogging.getMaatId().toString() : "").build());
 
         MDC.put(MESSAGE, laaTransactionLogging.toString());
         MDC.put(CASE_URN, laaTransactionLogging.getCaseUrn());

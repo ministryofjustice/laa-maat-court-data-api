bee84c4aa934840675bad32a32f038dac27e6a28
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk.com>
2020-07-28 20:04:35 +0100

    Enabling Mutation Testing for Linking Functionality

---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/link/controller/LinkControllerTest.java
@@ -0,0 +1,35 @@
+package gov.uk.courtdata.link.controller;
+
+import gov.uk.courtdata.link.validator.PreConditionsValidator;
+import gov.uk.courtdata.model.CaseDetailsValidate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LinkControllerTest {
+
+    @InjectMocks
+    private LinkController linkController;
+
+    @Mock
+    private PreConditionsValidator preConditionsValidator;
+
+    @Test
+    public void givenCaseValidationDetails_whenValidateIsInvoked_thenValidationPerformed() {
+        CaseDetailsValidate caseDetailsValidate = CaseDetailsValidate.builder().caseUrn("1244").maatId(1234).build();
+        ResponseEntity<Object> x = linkController.validate(caseDetailsValidate);
+
+        verify(preConditionsValidator, times(1)).validate(caseDetailsValidate);
+        assertThat(x).isEqualTo(ResponseEntity.ok().build());
+
+    }
+
+}
---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/link/impl/SaveAndLinkImplTest.java
@@ -0,0 +1,81 @@
+package gov.uk.courtdata.link.impl;
+
+import gov.uk.courtdata.dto.CourtDataDTO;
+import gov.uk.courtdata.link.processor.*;
+import gov.uk.courtdata.repository.IdentifierRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SaveAndLinkImplTest {
+
+    @InjectMocks
+    private SaveAndLinkImpl saveAndLink;
+
+    @Mock
+    private CaseInfoProcessor caseInfoProcessor;
+    @Mock
+    private WqCoreInfoProcessor wqCoreInfoProcessor;
+    @Mock
+    private WqLinkRegisterProcessor wqLinkRegisterProcessor;
+    @Mock
+    private SolicitorInfoProcessor solicitorInfoProcessor;
+    @Mock
+    private ProceedingsInfoProcessor proceedingsInfoProcessor;
+    @Mock
+    private DefendantInfoProcessor defendantInfoProcessor;
+    @Mock
+    private SessionInfoProcessor sessionInfoProcessor;
+    @Mock
+    private OffenceInfoProcessor offenceInfoProcessor;
+    @Mock
+    private ResultsInfoProcessor resultsInfoProcessor;
+    @Mock
+    private RepOrderCPInfoProcessor repOrderCPInfoProcessor;
+    @Mock
+    private IdentifierRepository identifierRepository;
+    @Mock
+    private RepOrderInfoProcessor repOrderInfoProcessor;
+
+    @Test
+    public void givenCaseDetails_whenExecuteIsInvoked_thenCaseIsLinked() {
+
+        //given
+        CourtDataDTO courtDataDTO = CourtDataDTO.builder().build();
+
+        //when
+        when(identifierRepository.getCaseID()).thenReturn(1);
+        when(identifierRepository.getTxnID()).thenReturn(1);
+        when(identifierRepository.getProceedingID()).thenReturn(1);
+        when(identifierRepository.getLibraID()).thenReturn(1);
+
+        saveAndLink.execute(courtDataDTO);
+
+        //then
+        verify(caseInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(wqCoreInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(wqLinkRegisterProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(solicitorInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(proceedingsInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(defendantInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(sessionInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(offenceInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(resultsInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(repOrderCPInfoProcessor, atLeastOnce()).process(courtDataDTO);
+        verify(repOrderInfoProcessor, atLeastOnce()).process(courtDataDTO);
+
+        assertThat(courtDataDTO.getCaseId()).isEqualTo(1);
+        assertThat(courtDataDTO.getProceedingId()).isEqualTo(1);
+        assertThat(courtDataDTO.getTxId()).isEqualTo(1);
+        assertThat(courtDataDTO.getLibraId()).isEqualTo("CP1");
+
+
+    }
+
+}

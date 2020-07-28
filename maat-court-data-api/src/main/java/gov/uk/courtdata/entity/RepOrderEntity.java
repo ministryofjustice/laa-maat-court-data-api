a1f0290f9d2a8278463acb6230042052cfcf3ecd
Praveen Raju <praveen.raju@digital.justice.gov.uk>
2020-07-28 23:02:43 +0100

    Add named stored procs to handle null params

--- maat-court-data-api/src/main/java/gov/uk/courtdata/entity/RepOrderEntity.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/entity/RepOrderEntity.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,6 +15,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 @Table(name = "REP_ORDERS", schema = "TOGDATA")
+@NamedStoredProcedureQuery(
+        name = "update_cc_outcome",
+        procedureName = "togdata.application.update_cc_outcome",
+        parameters = {
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = Integer.class, name = "p_rep_id"),
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "p_cc_outcome"),
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "p_bench_warrant_issued"),
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "p_appeal_type"),
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "p_imprisoned"),
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "p_case_number"),
+                @StoredProcedureParameter(mode = ParameterMode.IN, type = String.class, name = "p_crown_court_code")
+        }
+)
 public class RepOrderEntity {
 
     @Id
---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/CrownCourtStoredProcedureRepository.java
@@ -0,0 +1,47 @@
+package gov.uk.courtdata.repository;
+
+import org.hibernate.Session;
+import org.hibernate.procedure.ProcedureCall;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class CrownCourtStoredProcedureRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void updateCrownCourtOutcome(Integer repId,
+                                        String ccOutcome,
+                                        String benchWarrantIssued,
+                                        String appealType,
+                                        String imprisoned,
+                                        String caseNumber,
+                                        String crownCourtCode) {
+
+
+        final Session session = entityManager.unwrap(Session.class);
+
+        final ProcedureCall ccOutcomeProcedure = session.getNamedProcedureCall("update_cc_outcome");
+
+        ccOutcomeProcedure.getParameterRegistration("p_imprisoned").enablePassingNulls(true);
+        ccOutcomeProcedure.getParameterRegistration("p_bench_warrant_issued").enablePassingNulls(true);
+
+
+        ccOutcomeProcedure.setParameter("p_rep_id", repId);
+        ccOutcomeProcedure.setParameter("p_imprisoned", imprisoned);
+        ccOutcomeProcedure.setParameter("p_cc_outcome", ccOutcome);
+        ccOutcomeProcedure.setParameter("p_bench_warrant_issued", benchWarrantIssued);
+        ccOutcomeProcedure.setParameter("p_appeal_type", appealType);
+        ccOutcomeProcedure.setParameter("p_case_number", caseNumber);
+        ccOutcomeProcedure.setParameter("p_crown_court_code", crownCourtCode);
+
+
+        ccOutcomeProcedure.execute();
+
+    }
+}

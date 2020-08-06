78b04a5841baed399236aaf4e0cb9efb7747d7a5
atharmajeed <athar@tyrocircle.com>
2020-08-06 16:46:52 +0100

    Added Open API and swagger3 configuration. Made header value as a optional for rest calls.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/laastatus/controller/LaaStatusUpdateController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/laastatus/controller/LaaStatusUpdateController.java
@@ -6,17 +6,28 @@ import gov.uk.courtdata.laastatus.service.LaaStatusPublisher;
 import gov.uk.courtdata.laastatus.validator.LaaStatusValidationProcessor;
 import gov.uk.courtdata.model.CaseDetails;
 import gov.uk.courtdata.model.MessageCollection;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.UUID;
 
+import static gov.uk.courtdata.exception.GlobalAppLoggingHandler.LAA_TRANSACTION_ID;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("maatApi")
+@Tag(name = "LAA Status", description = "Rest APIs for Case linking")
 public class LaaStatusUpdateController {
 
     private final LaaStatusValidationProcessor laaStatusValidationProcessor;
@@ -24,15 +35,25 @@ public class LaaStatusUpdateController {
     private final LaaStatusPublisher laaStatusPublisher;
 
     @PostMapping("/laaStatus")
-    public MessageCollection updateLAAStatus(@RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId, @RequestBody String jsonPayload) {
+    @Operation(summary = "Process LAA Status updates.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageCollection.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request here", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server Error", content = @Content)
+    })
 
+    public MessageCollection updateLAAStatus(@RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId,
+                                             @Parameter(description = "Case details", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CaseDetails.class)))
+                                             @RequestBody String jsonPayload) {
+
+        MDC.put(LAA_TRANSACTION_ID, laaTransactionId);
         log.info("LAA Status Update Request received.");
         MessageCollection messageCollection;
         try {
             CaseDetails caseDetails = gson.fromJson(jsonPayload, CaseDetails.class);
 
-            UUID ud = Optional.ofNullable(laaTransactionId).isPresent() ? UUID.fromString(laaTransactionId) : UUID.randomUUID();
-            caseDetails.setLaaTransactionId(ud);
+            UUID laaTransactionIdUUID = Optional.ofNullable(laaTransactionId).isPresent() ? UUID.fromString(laaTransactionId) : UUID.randomUUID();
+            caseDetails.setLaaTransactionId(laaTransactionIdUUID);
 
             messageCollection = laaStatusValidationProcessor.validate(caseDetails);
 
--- maat-court-data-api/src/main/java/gov/uk/courtdata/unlink/controller/UnLinkController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/unlink/controller/UnLinkController.java
@@ -2,27 +2,46 @@ package gov.uk.courtdata.unlink.controller;
 
 import gov.uk.courtdata.model.Unlink;
 import gov.uk.courtdata.unlink.validator.UnLinkValidationProcessor;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static gov.uk.courtdata.exception.GlobalAppLoggingHandler.LAA_TRANSACTION_ID;
 
 @RestController
 @RequestMapping("/unlink")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "UnLinking", description = "Rest APIs for Case unlinking.")
 public class UnLinkController {
 
 
     private final UnLinkValidationProcessor unLinkValidationProcessor;
 
     @PostMapping("/validate")
-    public ResponseEntity<Object> validate(@RequestHeader("Laa-Transaction-Id") String laaTransactionId,
-                                           @RequestBody Unlink unlink) {
+    @Operation(description = "Unlinking case")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server Error", content = @Content)
+    })
+    public ResponseEntity<Object> validate(
+            @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId,
+            @Parameter(description = "Case details data", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Unlink.class)))
+            @RequestBody Unlink unlink) {
 
-        log.info("LAA Status Update Request received - laa-transaction-id:{}", laaTransactionId);
-
-        log.info("Request received: {}", unlink.toString());
+        MDC.put(LAA_TRANSACTION_ID, laaTransactionId);
+        log.info("LAA Status Update Request received");
         unLinkValidationProcessor.validate(unlink);
 
         return ResponseEntity.ok().build();

c6c7c06f933a8a1cb443b4af16c7d6325f4ba298
Don Masters <don@automationlogic.com>
2020-09-09 14:43:56 +0100

    LAWS-1020-add-sonarqube-testing-to-codepipeline
    - Change CodeBuild to run in a private Subnet
    - Set CodeBuild Security Group to allow access to SonarQube
    - Set CodeBuild to use a specific role, this role will allow access
    to specific Parameter Store secrets
    - In CodeBuild pass SonarQube user token as Environment Variable
    - Update build.sh to change Gradle to run SonarScanner
    
    These changes to maat-cd-api provide SonarQube as a static analysis
    tool every time the ECS image is built

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,10 @@ set -e
 
 echo Building the ear file and Docker image with gradle...
 pushd maat-court-data-api
-chmod +x ./gradlew && ./gradlew build
+chmod +x ./gradlew && ./gradlew sonarqube \
+  -Dsonar.projectKey=maat-cd-api \
+  -Dsonar.host.url=http://sonarqube.aws.ssvs.legalservices.gov.uk \
+  -Dsonar.login=${SONARQUBE_TOKEN}
 docker build -t maat-cda .
 docker tag maat-cda "${IMAGE_URI}"
 popd

6319efe178f0176fe0f1222c0879db8c21ba17db
Praveen Raju <praveen.raju@digital.justice.gov.uk>
2020-10-05 14:23:31 +0100

    Tidy up imports

--- maat-court-data-api/src/main/java/gov/uk/courtdata/entity/WqLinkRegisterEntity.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/entity/WqLinkRegisterEntity.java
@@ -1,9 +1,14 @@
 package gov.uk.courtdata.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Data

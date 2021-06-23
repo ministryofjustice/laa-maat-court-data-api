0d51777cad705381adf2c8ae6fddbcbb55473513
atharmajeed <athar.majeed@digital.justice.gov.uk>
2021-06-23 18:04:08 +0100

    upgraded sentry version to 5x. Added more meta-data for sentry and cloud watch logs

--- maat-court-data-api/src/main/java/gov/uk/courtdata/hearing/service/HearingResultedListener.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/hearing/service/HearingResultedListener.java
@@ -1,11 +1,13 @@
 package gov.uk.courtdata.hearing.service;
 
 import com.google.gson.Gson;
+import gov.uk.courtdata.enums.LoggingData;
 import gov.uk.courtdata.enums.MessageType;
 import gov.uk.courtdata.model.hearing.HearingResulted;
 import gov.uk.courtdata.service.QueueMessageLogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -24,10 +26,9 @@ public class HearingResultedListener {
     @JmsListener(destination = "${cloud-platform.aws.sqs.queue.hearingResulted}")
     public void receive(@Payload final String message) {
 
+        MDC.put(LoggingData.REQUEST_TYPE.getValue(), MessageType.HEARING.name());
         queueMessageLogService.createLog(MessageType.HEARING,message);
         HearingResulted hearingResulted = gson.fromJson(message, HearingResulted.class);
         hearingResultedService.execute(hearingResulted);
     }
 }
-
-

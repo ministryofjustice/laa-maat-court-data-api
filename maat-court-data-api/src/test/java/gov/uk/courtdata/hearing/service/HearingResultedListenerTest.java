3b9faf59cb9f78152a24c976931b5378d9ea33b9
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-11-15 09:49:54 +0000

    persisted JurisdictionType against MAATID/Hearing ID when HearingResultedListener.receive() is called

--- maat-court-data-api/src/test/java/gov/uk/courtdata/hearing/service/HearingResultedListenerTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/hearing/service/HearingResultedListenerTest.java
@@ -1,18 +1,19 @@
 package gov.uk.courtdata.hearing.service;
 
 import com.google.gson.Gson;
+import gov.uk.courtdata.enums.JurisdictionType;
 import gov.uk.courtdata.enums.MessageType;
 import gov.uk.courtdata.model.hearing.HearingResulted;
 import gov.uk.courtdata.service.QueueMessageLogService;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HearingResultedListenerTest {
@@ -27,7 +28,6 @@ public class HearingResultedListenerTest {
     @Mock
     private QueueMessageLogService queueMessageLogService;
 
-
     @BeforeEach
     public void setUp() {
 
@@ -38,14 +38,15 @@ public class HearingResultedListenerTest {
     public void givenJSONMessageIsReceived_whenHearingResultedListenerIsInvoked_thenHearingResultedServiceIsCalled() {
         //given
         HearingResulted laaHearingDetails = HearingResulted.builder().build();
-        String message = "Test JSON";
+        String message = "{\"laaTransactionId\":\"c77c96ff-7cad-44cc-9e12-5bc80f5f2d9e\" ,\n" +
+                "  \"caseUrn\":\"CASNUM-ABC123\",\n" +
+                "  \"maatId\": \"null\"}";
         //when
         when(gson.fromJson(message, HearingResulted.class)).thenReturn(laaHearingDetails);
         hearingResultedListener.receive(message);
         //then
         verify(hearingResultedService, times(1)).execute(laaHearingDetails);
         verify(queueMessageLogService, times(1)).createLog(MessageType.HEARING, message);
+        assertNotNull(laaHearingDetails.getHearingId());
     }
-
-
 }

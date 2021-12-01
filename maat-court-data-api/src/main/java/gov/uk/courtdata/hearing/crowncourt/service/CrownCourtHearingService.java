279496689e1b66fce65731f5887a3f8390b26b89
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk>
2021-12-01 16:58:59 +0000

    Adding a temp change to data fix CC Outcomes

--- maat-court-data-api/src/main/java/gov/uk/courtdata/hearing/crowncourt/service/CrownCourtHearingService.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/hearing/crowncourt/service/CrownCourtHearingService.java
@@ -1,22 +1,13 @@
 package gov.uk.courtdata.hearing.crowncourt.service;
 
-import gov.uk.courtdata.enums.CrownCourtTrialOutcome;
-import gov.uk.courtdata.enums.PleaTrialOutcome;
-import gov.uk.courtdata.enums.VerdictTrialOutcome;
 import gov.uk.courtdata.hearing.crowncourt.impl.CrownCourtProcessingImpl;
-import gov.uk.courtdata.hearing.crowncourt.impl.OffenceHelper;
 import gov.uk.courtdata.hearing.crowncourt.validator.CrownCourtValidationProcessor;
 import gov.uk.courtdata.hearing.impl.HearingResultedImpl;
-import gov.uk.courtdata.model.Offence;
 import gov.uk.courtdata.model.hearing.CCOutComeData;
 import gov.uk.courtdata.model.hearing.HearingResulted;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -32,10 +23,8 @@ public class CrownCourtHearingService {
 
         hearingResultedImpl.execute(hearingResulted);
 
-        if (isCaseConcluded(hearingResulted)) {
+        executeCrownCourtOutCome(hearingResulted);
 
-            executeCrownCourtOutCome(hearingResulted);
-        }
     }
 
     private boolean isCaseConcluded(HearingResulted hearingResulted) {
@@ -48,12 +37,13 @@ public class CrownCourtHearingService {
     }
 
 
-    private void executeCrownCourtOutCome(HearingResulted hearingResulted) {
+    public void executeCrownCourtOutCome(HearingResulted hearingResulted) {
 
-        crownCourtValidationProcessor.validate(hearingResulted);
-        crownCourtProcessingImpl.execute(hearingResulted);
-        log.info("Crown Court Outcome Processing has been Completed for MAAT ID: {}", hearingResulted.getMaatId());
+        if (isCaseConcluded(hearingResulted)) {
+            crownCourtValidationProcessor.validate(hearingResulted);
+            crownCourtProcessingImpl.execute(hearingResulted);
+            log.info("Crown Court Outcome Processing has been Completed for MAAT ID: {}", hearingResulted.getMaatId());
+        }
     }
-
 
 }

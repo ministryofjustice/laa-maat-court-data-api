44d2bb0972d63ec98dfc7d34005bde2221b0b308
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-01-07 15:41:12 +0000

    Fix missing import statement

--- maat-court-data-api/src/main/java/gov/uk/courtdata/authorization/controller/AuthorizationController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/authorization/controller/AuthorizationController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;

fb02ec1169debb7bf9d11ba34e0e9f8690535ac0
atharmajeed <athar.majeed@digital.justice.gov.uk>
2022-02-02 17:47:39 +0000

    Changing the data-type for defendant-id from integer to UUID.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/prosecutionconcluded/listner/request/ProsecutionConcluded.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/prosecutionconcluded/listner/request/ProsecutionConcluded.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ProsecutionConcluded {
     private Integer maatId;
-    private Integer defendantId;
+    private UUID defendantId;
     private UUID prosecutionCaseId;
     private boolean concluded;
     private UUID hearingIdWhereChangeOccurred;

34fe20e053282376fe3ca4b59731a40216275697
Aafreen Ansari <aafreenansari@gmail.com>
2022-02-07 16:48:44 +0000

    LASB-843|added service class for child weightings

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/impl/ChildWeightingsImpl.java
@@ -0,0 +1,39 @@
+package gov.uk.courtdata.assessment.impl;
+
+import gov.uk.courtdata.assessment.mapper.FinancialAssessmentMapper;
+import gov.uk.courtdata.entity.ChildWeightingsEntity;
+import gov.uk.courtdata.entity.FinancialAssessmentEntity;
+import gov.uk.courtdata.model.assessment.ChildWeightings;
+import gov.uk.courtdata.repository.ChildWeightingsRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toList;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChildWeightingsImpl {
+    private final FinancialAssessmentMapper assessmentMapper;
+    private final ChildWeightingsRepository childWeightingsRepository;
+
+    public List<ChildWeightingsEntity> save(final FinancialAssessmentEntity financialAssessment, final List<ChildWeightings> childWeightingsList) {
+        final List<ChildWeightingsEntity> childWeightingsEntityList = ofNullable(childWeightingsList)
+                .orElse(Collections.emptyList()).stream()
+                .map(childWeightings -> {
+                    ChildWeightingsEntity childWeightingsEntity =
+                            assessmentMapper.ChildWeightingsToChildWeightingsEntity(childWeightings);
+                    childWeightingsEntity.setFinancialAssessmentId(financialAssessment.getId());
+                    childWeightingsEntity.setUserCreated(financialAssessment.getUserCreated());
+                    childWeightingsEntity.setUserModified(financialAssessment.getUserModified());
+                    return childWeightingsEntity;
+                }).collect(toList());
+        return childWeightingsRepository.saveAll(childWeightingsEntityList);
+    }
+}
+

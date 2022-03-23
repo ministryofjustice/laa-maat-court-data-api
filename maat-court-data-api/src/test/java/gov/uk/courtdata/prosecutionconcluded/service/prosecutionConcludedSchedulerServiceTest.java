496773df214a8af72596a73fee17cbbd202d756f
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk>
2022-03-23 20:55:20 +0000

    fixing the build issue

--- maat-court-data-api/src/test/java/gov/uk/courtdata/prosecutionconcluded/service/prosecutionConcludedSchedulerServiceTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/prosecutionconcluded/service/prosecutionConcludedSchedulerServiceTest.java
@@ -2,7 +2,9 @@ package gov.uk.courtdata.prosecutionconcluded.service;
 
 import com.google.gson.Gson;
 import gov.uk.courtdata.entity.ProsecutionConcludedEntity;
+import gov.uk.courtdata.entity.WQHearingEntity;
 import gov.uk.courtdata.repository.ProsecutionConcludedRepository;
+import gov.uk.courtdata.repository.WQHearingRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class prosecutionConcludedSchedulerServiceTest {
@@ -24,6 +27,8 @@ public class prosecutionConcludedSchedulerServiceTest {
     private ProsecutionConcludedRepository prosecutionConcludedRepository;
     @Mock
     private ProsecutionConcludedService prosecutionConcludedService;
+    @Mock
+    private WQHearingRepository wqHearingRepository;
 
     @Mock
     private Gson gson;
@@ -37,6 +42,8 @@ public class prosecutionConcludedSchedulerServiceTest {
                 .maatId(1234)
                 .caseData("test".getBytes(StandardCharsets.UTF_8))
                 .build()));
+        when(wqHearingRepository.findByMaatIdAndHearingUUID(any(),any())).
+                thenReturn(List.of(WQHearingEntity.builder().wqJurisdictionType("CROWN").build()));
 
         //when
         prosecutionConcludedSchedulerService.process();

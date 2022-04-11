fd045f2cc22a81975db1ce6998ab040ef455c22b
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2022-04-11 18:57:59 +0100

    LASB-1014 - fix failing tests

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/ChildWeightHistoryRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/ChildWeightHistoryRepository.java
@@ -3,8 +3,6 @@ package gov.uk.courtdata.repository;
 import gov.uk.courtdata.entity.ChildWeightHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ChildWeightHistoryRepository extends JpaRepository<ChildWeightHistoryEntity, Integer> {
-    List<ChildWeightHistoryEntity> findAllByFinancialAssessmentId(Integer financialAssessmentId);
+//    List<ChildWeightHistoryEntity> findAllByFinancialAssessmentId(Integer financialAssessmentId);
 }
--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/FinancialAssessmentDetailsHistoryRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/FinancialAssessmentDetailsHistoryRepository.java
@@ -3,8 +3,6 @@ package gov.uk.courtdata.repository;
 import gov.uk.courtdata.entity.FinancialAssessmentDetailsHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FinancialAssessmentDetailsHistoryRepository extends JpaRepository<FinancialAssessmentDetailsHistoryEntity, Integer> {
-    List<FinancialAssessmentDetailsHistoryEntity> findAllByFinancialAssessmentId(Integer financialAssessmentId);
+//    List<FinancialAssessmentDetailsHistoryEntity> findAllByFinancialAssessmentId(Integer financialAssessmentId);
 }

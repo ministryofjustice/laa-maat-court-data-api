1d79102b551073a166aac90759a77e60cf3a2f84
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-05-03 14:09:11 +0100

    Added new endpoint for handling assessment post-processing

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/service/PostProcessingService.java
@@ -0,0 +1,25 @@
+package gov.uk.courtdata.assessment.service;
+
+import gov.uk.courtdata.repository.PostProcessingStoredProcedureRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostProcessingService {
+
+    private final PostProcessingStoredProcedureRepository postProcessingStoredProcedureRepository;
+
+    @Transactional
+    public void execute(Integer repId) {
+        try {
+            postProcessingStoredProcedureRepository.invokePostAssessmentProcessingCma(repId);
+        } catch (DataAccessException exception) {
+            log.error("Post-processing failed for repId: {}", repId);
+        }
+    }
+}

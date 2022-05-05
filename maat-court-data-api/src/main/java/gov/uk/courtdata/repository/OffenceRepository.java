ed2b18ca3b91d719cb55ba5ce1226f26c4c1b830
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-05-05 10:09:03 +0100

    LASB-1056: Corrected NUMROW to ROWNUM in offence query.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/OffenceRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/OffenceRepository.java
@@ -23,13 +23,13 @@ public interface OffenceRepository extends JpaRepository<OffenceEntity, AsnSeqTx
     @Query(value = "SELECT COUNT(*) FROM MLA.XXMLA_OFFENCE WHERE CASE_ID = ?1 AND ASN_SEQ = ?2", nativeQuery = true)
     Integer getOffenceCountForAsnSeq(Integer caseId, String asnSeq);
 
-    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE NUMROW = 1 AND CASE_ID = :caseId AND ASN_SEQ = :asnSeq " +
+    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE ROWNUM = 1 AND CASE_ID = :caseId AND ASN_SEQ = :asnSeq " +
                     "AND OFFENCE_CODE = :offenceCode ORDER BY TX_ID DESC", nativeQuery = true)
     Optional<OffenceEntity> findByMaxTxId(Integer caseId, String offenceCode, String asnSeq);
 
     List<OffenceEntity> findByCaseId(Integer caseId);
 
-    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE NUMROW = 1 AND CASE_ID = :caseId AND APPLICATION_FLAG = :applicationFlag " +
+    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE ROWNUM = 1 AND CASE_ID = :caseId AND APPLICATION_FLAG = :applicationFlag " +
                     "AND OFFENCE_ID = :offenceId ORDER BY TX_ID DESC", nativeQuery = true)
     Optional<OffenceEntity> findApplicationByOffenceCode(Integer caseId, String offenceId, Integer applicationFlag);
 

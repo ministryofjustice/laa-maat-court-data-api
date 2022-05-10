7f075641f25f00ccd52c56e69ed957a5bf3c7369
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-05-10 13:18:43 +0100

    Centralised response error codes

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/constants/ErrorCodes.java
@@ -0,0 +1,7 @@
+package gov.uk.courtdata.constants;
+
+public final class ErrorCodes {
+
+    public static final String DB_ERROR = "DB Error";
+    public static final String OBJECT_NOT_FOUND = "Object Not Found";
+}

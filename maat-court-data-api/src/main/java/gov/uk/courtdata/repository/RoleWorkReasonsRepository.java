cce2506260fff851e383c45aaf078b1ecaa258ee
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-05-13 09:05:59 +0100

    LASB-1072: Added reference to the TOGDATA schema in Role and Reservation queries.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/RoleWorkReasonsRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/RoleWorkReasonsRepository.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface RoleWorkReasonsRepository extends JpaRepository<RoleWorkReasonEntity, Integer> {
-    @Query(value = "select wr.* from ROLE_WORK_REASONS wr inner join USER_ROLES ur on (wr.ROLE_NAME = ur.ROLE_NAME) where  ur.USER_NAME = :username and wr.NWOR_CODE = :nworCode FETCH FIRST 1 ROWS ONLY", nativeQuery = true)
+    @Query(value = "select wr.* from TOGDATA.ROLE_WORK_REASONS wr inner join TOGDATA.USER_ROLES ur on (wr.ROLE_NAME = ur.ROLE_NAME) where  ur.USER_NAME = :username and wr.NWOR_CODE = :nworCode FETCH FIRST 1 ROWS ONLY", nativeQuery = true)
     Optional<RoleWorkReasonEntity> getNewWorkReason(@Param("username") String username, @Param("nworCode") String nworCode);
 }

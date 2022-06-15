c593bcb02d828615b334d85df493a9ef096144e3
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-06-15 09:57:23 +0100

    Revert "Merge pull request #515 from ministryofjustice/LASB-999-Setup-reporting-of-actuator-metrics-to-cloudwatch"
    This reverts commit b234095711d4990e383cf478d8cb26715cad5e62, reversing
    changes made to f35a6d6c54584dd0e8b0127795c7abc9a6e2292c.

--- maat-court-data-api/src/main/java/gov/uk/MAATCourtDataApplication.java
+++ maat-court-data-api/src/main/java/gov/uk/MAATCourtDataApplication.java
@@ -2,18 +2,9 @@ package gov.uk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.*;
-import org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication(exclude = {
-        ContextCredentialsAutoConfiguration.class,
-        ContextInstanceDataAutoConfiguration.class,
-        ContextRegionProviderAutoConfiguration.class,
-        ContextResourceLoaderAutoConfiguration.class,
-        ContextStackAutoConfiguration.class,
-        MailSenderAutoConfiguration.class,
-})
+@SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class MAATCourtDataApplication {
 

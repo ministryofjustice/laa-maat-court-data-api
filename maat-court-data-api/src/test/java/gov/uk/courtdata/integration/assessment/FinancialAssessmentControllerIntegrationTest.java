57fad33f8cd9fa8d502433ba00468792e71fe3bc
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-06-23 13:13:19 +0100

    LASB-1096: Integration tests added for the createFinancialAssessmentHistory endpoint. enable_lazy_load_no_trans hibernate parameter set to true for tests to allow lazy loading of data in tests without transaction management.

--- maat-court-data-api/src/test/java/gov/uk/courtdata/integration/assessment/FinancialAssessmentControllerIntegrationTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/integration/assessment/FinancialAssessmentControllerIntegrationTest.java
@@ -3,20 +3,19 @@ package gov.uk.courtdata.integration.assessment;
 import gov.uk.MAATCourtDataApplication;
 import gov.uk.courtdata.assessment.impl.FinancialAssessmentImpl;
 import gov.uk.courtdata.assessment.mapper.FinancialAssessmentMapper;
+import gov.uk.courtdata.builder.TestEntityDataBuilder;
 import gov.uk.courtdata.builder.TestModelDataBuilder;
 import gov.uk.courtdata.dto.FinancialAssessmentDTO;
 import gov.uk.courtdata.dto.OutstandingAssessmentResultDTO;
 import gov.uk.courtdata.entity.*;
 import gov.uk.courtdata.integration.MockServicesConfig;
 import gov.uk.courtdata.model.NewWorkReason;
-import gov.uk.courtdata.model.assessment.ChildWeightings;
-import gov.uk.courtdata.model.assessment.CreateFinancialAssessment;
-import gov.uk.courtdata.model.assessment.FinancialAssessmentDetails;
-import gov.uk.courtdata.model.assessment.UpdateFinancialAssessment;
+import gov.uk.courtdata.model.assessment.*;
 import gov.uk.courtdata.repository.*;
 import gov.uk.courtdata.util.MockMvcIntegrationTest;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,8 +57,17 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
     private ChildWeightingsRepository childWeightingsRepository;
     @Autowired
     private FinancialAssessmentDetailsRepository financialAssessmentDetailsRepository;
+    @Autowired
+    private RepOrderRepository repOrderRepository;
+    @Autowired
+    private ChildWeightHistoryRepository childWeightHistoryRepository;
+    @Autowired
+    private FinancialAssessmentsHistoryRepository financialAssessmentsHistoryRepository;
+    @Autowired
+    private FinancialAssessmentDetailsHistoryRepository financialAssessmentDetailsHistoryRepository;
 
     private List<FinancialAssessmentEntity> existingAssessmentEntities;
+    private RepOrderEntity existingRepOrder;
 
     @Before
     @Override
@@ -69,15 +77,33 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
     }
 
     private void setupTestData() {
-        financialAssessmentRepository.deleteAll();
+
         hardshipReviewRepository.deleteAll();
         passportAssessmentRepository.deleteAll();
-        newWorkReasonRepository.deleteAll();
         childWeightingsRepository.deleteAll();
         financialAssessmentDetailsRepository.deleteAll();
+        financialAssessmentsHistoryRepository.deleteAll();
+        financialAssessmentDetailsHistoryRepository.deleteAll();
+        childWeightHistoryRepository.deleteAll();
+        financialAssessmentRepository.deleteAll();
+        newWorkReasonRepository.deleteAll();
 
         LocalDateTime testCreationDate = LocalDateTime.of(2022, 1, 1, 12, 0);
-        String testUser = "test-user";
+        String testUser = "test-f";
+
+        existingRepOrder = repOrderRepository.save(
+                RepOrderEntity.builder()
+                        .id(4444)
+                        .catyCaseType("case-type")
+                        .magsOutcome("outcome")
+                        .magsOutcomeDate(testCreationDate.toString())
+                        .magsOutcomeDateSet(testCreationDate.toLocalDate())
+                        .committalDate(testCreationDate.toLocalDate())
+                        .rderCode("rder-code")
+                        .ccRepDec("cc-rep-doc")
+                        .ccRepType("cc-rep-type")
+                        .build()
+        );
 
         NewWorkReasonEntity newWorkReasonEntity = newWorkReasonRepository.save(
                 NewWorkReasonEntity.builder()
@@ -87,6 +113,24 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
                         .dateCreated(testCreationDate)
                         .userCreated(testUser)
                         .build());
+
+        var assessmentDetails = TestEntityDataBuilder.getFinancialAssessmentDetailsEntity();
+        assessmentDetails.setId(null);
+
+        FinancialAssessmentEntity assessmentWithWeightingsAndDetails = FinancialAssessmentEntity.builder()
+                .repId(existingRepOrder.getId())
+                .assessmentType("INIT")
+                .fassFullStatus("COMPLETE")
+                .dateCreated(testCreationDate)
+                .userCreated(testUser)
+                .initialAscrId(10)
+                .usn(11)
+                .cmuId(12)
+                .replaced("N")
+                .newWorkReason(newWorkReasonEntity)
+                .build();
+        assessmentWithWeightingsAndDetails.addAssessmentDetail(assessmentDetails);
+        assessmentWithWeightingsAndDetails.addChildWeighting(TestEntityDataBuilder.getChildWeightingsEntity());
 
         List<FinancialAssessmentEntity> assessmentsToCreate = List.of(
 
@@ -122,7 +166,8 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
                         .cmuId(9)
                         .replaced("N")
                         .newWorkReason(newWorkReasonEntity)
-                        .build()
+                        .build(),
+                assessmentWithWeightingsAndDetails
         );
 
         existingAssessmentEntities = financialAssessmentRepository.saveAll(assessmentsToCreate);
@@ -231,8 +276,12 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
         CreateFinancialAssessment body = TestModelDataBuilder.getCreateFinancialAssessmentWithRelationships();
         body.setRepId(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS);
 
-        FinancialAssessmentDTO expectedResponse = assessmentMapper.CreateFinancialAssessmentToFinancialAssessmentDTO(body);
+        FinancialAssessmentDTO expectedResponse = TestModelDataBuilder.getFinancialAssessmentDTO();
+        expectedResponse.setRepId(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS);
+        expectedResponse.setChildWeightings(List.of(TestModelDataBuilder.getChildWeightings()));
+        expectedResponse.setAssessmentDetails(List.of(TestModelDataBuilder.getFinancialAssessmentDetails()));
         expectedResponse.setAssessmentType("INIT");
+        expectedResponse.setInitialAssessmentDate(body.getInitialAssessmentDate());
 
         MvcResult result =
                 runSuccessScenario(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(body)));
@@ -246,13 +295,6 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
         FinancialAssessmentEntity createdAssessment =
                 matchingAssessments.stream().filter(assessment -> assessment.getReplaced().equals("N")).collect(Collectors.toList()).get(0);
 
-        List<ChildWeightingsEntity> childWeightings =
-                childWeightingsRepository.findAll().stream()
-                        .filter(item -> item.getFinancialAssessment().getRepId().equals(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS))
-                        .collect(Collectors.toList());
-        List<FinancialAssessmentDetailEntity> assessmentDetails =
-                financialAssessmentDetailsRepository.findAllByFinancialAssessmentId(createdAssessment.getId());
-
         expectedResponse.setId(createdAssessment.getId());
         expectedResponse.setDateCreated(createdAssessment.getDateCreated());
         expectedResponse.setUpdated(createdAssessment.getUpdated());
@@ -261,8 +303,8 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
             assertThat(matchingAssessments.size()).isEqualTo(2);
             assertThat((int) matchingAssessments.stream().filter(assessment -> assessment.getReplaced().equals("Y")).count())
                     .isEqualTo(1);
-            assertChildWeightingsEqual(expectedResponse.getChildWeightings(), childWeightings);
-            assertFinancialAssessmentDetailsEqual(expectedResponse.getAssessmentDetails(), assessmentDetails);
+            assertChildWeightingsEqual(expectedResponse.getChildWeightings(), createdAssessment.getChildWeightings());
+            assertFinancialAssessmentDetailsEqual(expectedResponse.getAssessmentDetails(), createdAssessment.getAssessmentDetails());
             assertFinancialAssessmentEqual(expectedResponse, createdAssessment);
         });
 
@@ -315,6 +357,7 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
     public void givenAValidAssessmentBody_whenUpdateAssessmentIsInvoked_theCorrectResponseIsReturned() throws Exception {
         FinancialAssessmentEntity assessmentToUpdate =
                 existingAssessmentEntities.stream().filter(item -> item.getRepId().equals(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS)).findFirst().orElse(null);
+
         Integer assessmentId = Objects.requireNonNull(assessmentToUpdate).getId();
         UpdateFinancialAssessment body = TestModelDataBuilder.getUpdateFinancialAssessment();
         body.setChildWeightings(List.of(TestModelDataBuilder.getChildWeightings()));
@@ -322,8 +365,14 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
         body.setRepId(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS);
         body.setId(assessmentId);
 
-        FinancialAssessmentDTO expectedResponse = assessmentMapper.UpdateFinancialAssessmentToFinancialAssessmentDTO(body);
+        FinancialAssessmentDTO expectedResponse = TestModelDataBuilder.getFinancialAssessmentDTO();
+        expectedResponse.setId(assessmentToUpdate.getId());
+        expectedResponse.setUserModified(body.getUserModified());
+        expectedResponse.setRepId(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS);
+        expectedResponse.setChildWeightings(List.of(TestModelDataBuilder.getChildWeightings()));
+        expectedResponse.setAssessmentDetails(List.of(TestModelDataBuilder.getFinancialAssessmentDetails()));
         expectedResponse.setAssessmentType("INIT");
+        expectedResponse.setInitialAssessmentDate(body.getInitialAssessmentDate());
         expectedResponse.setNewWorkReason(NewWorkReason.builder().code(assessmentToUpdate.getNewWorkReason().getCode()).build());
         expectedResponse.setUserCreated(assessmentToUpdate.getUserCreated());
         expectedResponse.setDateCreated(assessmentToUpdate.getDateCreated());
@@ -335,24 +384,81 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
 
         FinancialAssessmentEntity updatedAssessment = financialAssessmentRepository.findById(assessmentToUpdate.getId()).orElse(null);
 
-        List<ChildWeightingsEntity> childWeightings =
-                childWeightingsRepository.findAll().stream()
-                        .filter(item -> item.getFinancialAssessment().getRepId().equals(REP_ID_WITH_NO_OUTSTANDING_ASSESSMENTS))
-                        .collect(Collectors.toList());
-        List<FinancialAssessmentDetailEntity> assessmentDetails =
-                financialAssessmentDetailsRepository.findAllByFinancialAssessmentId(assessmentId);
-
         assertThat(assessmentToUpdate.getUpdated()).isNotEqualTo(Objects.requireNonNull(updatedAssessment).getUpdated());
         expectedResponse.setUpdated(updatedAssessment.getUpdated());
 
         SoftAssertions.assertSoftly(softly -> {
             assertThat(updatedAssessment).isNotNull();
-            assertChildWeightingsEqual(expectedResponse.getChildWeightings(), childWeightings);
-            assertFinancialAssessmentDetailsEqual(expectedResponse.getAssessmentDetails(), assessmentDetails);
+            assertChildWeightingsEqual(expectedResponse.getChildWeightings(), updatedAssessment.getChildWeightings());
+            assertFinancialAssessmentDetailsEqual(expectedResponse.getAssessmentDetails(), updatedAssessment.getAssessmentDetails());
             assertFinancialAssessmentEqual(expectedResponse, updatedAssessment);
         });
 
         assertThat(objectMapper.writeValueAsString(expectedResponse)).isEqualTo(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Ignore("This test will fail until LCAM-85 is addressed.")
+    public void givenAValidAssessmentIdWithoutAFullAssessmentAvailable_whenCreateAssessmentHistoryIsInvoked_theCorrectResponseIsReturned() throws Exception {
+        runCreateAssessmentHistoryScenario(false);
+    }
+
+    @Test
+    @Ignore("This test will fail until LCAM-85 is addressed.")
+    public void givenAValidAssessmentIdWithAFullAssessmentAvailable_whenCreateAssessmentHistoryIsInvoked_theCorrectResponseIsReturned() throws Exception {
+        runCreateAssessmentHistoryScenario(true);
+    }
+
+    private void runCreateAssessmentHistoryScenario(Boolean fullAvailable) throws Exception {
+        FinancialAssessmentEntity assessmentEntity =
+                existingAssessmentEntities.stream().filter(item -> item.getRepId().equals(existingRepOrder.getId())).findFirst().orElse(null);
+
+        String CREATE_ASSESSMENT_HISTORY_URL = BASE_URL + "history/{financialAssessmentId}/fullAvailable/{fullAvailable}";
+        runSuccessScenario(post(CREATE_ASSESSMENT_HISTORY_URL, Objects.requireNonNull(assessmentEntity).getId(), fullAvailable));
+        assertAssessmentHistoryCreated(assessmentEntity, fullAvailable, existingRepOrder);
+    }
+
+    public void assertAssessmentHistoryCreated(
+            FinancialAssessmentEntity assessmentEntity, Boolean fullAvailable, RepOrderEntity existingRepOrder) {
+
+        var createdHistoryEntities = financialAssessmentsHistoryRepository.findAll();
+        assertThat(createdHistoryEntities.size()).isEqualTo(1);
+        var createdHistory = createdHistoryEntities.get(0);
+
+        assertThat(createdHistory.getFinancialAssessment().getId()).isEqualTo(assessmentEntity.getId());
+        assertThat(createdHistory.getFullAvailable()).isEqualTo(fullAvailable ? "Y" : "N");
+        assertThat(createdHistory.getRepId()).isEqualTo(assessmentEntity.getRepId());
+        assertThat(createdHistory.getInitialAscrId()).isEqualTo(assessmentEntity.getInitialAscrId());
+        assertThat(createdHistory.getAssessmentType()).isEqualTo(assessmentEntity.getAssessmentType());
+        assertThat(createdHistory.getNewWorkReason().getCode()).isEqualTo(assessmentEntity.getNewWorkReason().getCode());
+        assertThat(createdHistory.getUserCreated()).isEqualTo(assessmentEntity.getUserCreated());
+        assertThat(createdHistory.getCmuId()).isEqualTo(assessmentEntity.getCmuId());
+        assertThat(createdHistory.getFassFullStatus()).isEqualTo(assessmentEntity.getFassFullStatus());
+        assertThat(createdHistory.getReplaced()).isEqualTo(assessmentEntity.getReplaced());
+        assertThat(createdHistory.getMagsOutcome()).isEqualTo(existingRepOrder.getMagsOutcome());
+        assertThat(createdHistory.getMagsOutcomeDate()).isEqualTo(existingRepOrder.getMagsOutcomeDate());
+        assertThat(createdHistory.getMagsOutcomeDateSet()).isEqualTo(existingRepOrder.getMagsOutcomeDateSet());
+        assertThat(createdHistory.getCommittalDate()).isEqualTo(existingRepOrder.getCommittalDate());
+        assertThat(createdHistory.getRderCode()).isEqualTo(existingRepOrder.getRderCode());
+        assertThat(createdHistory.getCcRepDec()).isEqualTo(existingRepOrder.getCcRepDec());
+        assertThat(createdHistory.getCcRepType()).isEqualTo(existingRepOrder.getCcRepType());
+        assertThat(createdHistory.getCaseType()).isEqualTo(existingRepOrder.getCatyCaseType());
+
+        // Check child weight history.
+        assertThat(createdHistory.getAssessmentDetails().size()).isEqualTo(1);
+        var childWeightHistory = createdHistory.getChildWeightings().get(0);
+        var expectedChildWeightHistory = TestEntityDataBuilder.getChildWeightHistoryEntity();
+        assertThat(childWeightHistory.getNoOfChildren()).isEqualTo(expectedChildWeightHistory.getNoOfChildren());
+        assertThat(childWeightHistory.getChildWeightingId()).isEqualTo(expectedChildWeightHistory.getChildWeightingId());
+
+        // Check assessment details history.
+        assertThat(createdHistory.getAssessmentDetails().size()).isEqualTo(1);
+        var assessmentDetailsHistory = createdHistory.getAssessmentDetails().get(0);
+        var expectedDetailsHistory = TestEntityDataBuilder.getFinancialAssessmentDetailsHistoryEntity();
+        assertThat(assessmentDetailsHistory.getCriteriaDetailId()).isEqualTo(expectedDetailsHistory.getCriteriaDetailId());
+        assertThat(assessmentDetailsHistory.getApplicantAmount()).isEqualByComparingTo(expectedDetailsHistory.getApplicantAmount().toString());
+        assertThat(assessmentDetailsHistory.getPartnerAmount()).isEqualByComparingTo(expectedDetailsHistory.getPartnerAmount().toString());
+        assertThat(assessmentDetailsHistory.getUserCreated()).isEqualTo(expectedDetailsHistory.getUserCreated());
     }
 
     private void runCreateAssessmentErrorScenario(String errorMessage, CreateFinancialAssessment body) throws Exception {
@@ -407,6 +513,4 @@ public class FinancialAssessmentControllerIntegrationTest extends MockMvcIntegra
         assertThat(expectedAssessment.getInitApplicationEmploymentStatus()).isEqualTo(actualAssessment.getInitApplicationEmploymentStatus());
         assertThat(expectedAssessment.getUpdated()).isEqualTo(actualAssessment.getUpdated());
     }
-
-
 }

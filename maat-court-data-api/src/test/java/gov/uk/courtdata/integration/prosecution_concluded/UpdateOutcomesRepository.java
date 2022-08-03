eba9442065331d40fa457e3964ecd66225997908
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-08-03 12:41:47 +0100

    LASB-1101: Modified the test crown court outcomes stored procedure to write the input values to a table in the test database so they can be verified.

---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/integration/prosecution_concluded/UpdateOutcomesRepository.java
@@ -0,0 +1,7 @@
+package gov.uk.courtdata.integration.prosecution_concluded;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UpdateOutcomesRepository extends JpaRepository<UpdateOutcomesEntity, String> {
+
+}

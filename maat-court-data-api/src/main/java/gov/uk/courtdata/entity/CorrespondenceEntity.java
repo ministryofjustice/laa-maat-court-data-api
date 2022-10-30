1aee34e0117302b1250d892cf9b88baacf896ae7
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2022-10-30 12:54:06 +0000

    LCAM-287-added entiry and repository

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/entity/CorrespondenceEntity.java
@@ -0,0 +1,50 @@
+package gov.uk.courtdata.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "CORRESPONDENCE", schema = "TOGDATA")
+public class CorrespondenceEntity {
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+    @Column(name = "REP_ID", nullable = false)
+    private Integer repId;
+    @Column(name = "GENERATE_DATE", nullable = false)
+    private LocalDateTime generateDate;
+    @Column(name = "PRINT_DATE")
+    private LocalDateTime printDate;
+    @Column(name = "DATE_CREATED", nullable = false)
+    private LocalDateTime dateCreated;
+    @Column(name = "USER_CREATED", nullable = false)
+    private String userCreated;
+    @Column(name = "DATE_MODIFIED")
+    private LocalDateTime dateModified;
+    @Column(name = "USER_MODIFIED")
+    private LocalDateTime userModified;
+    @Column(name = "COTY_CORRESPONDENCE_TYPE")
+    private String cotyCorresType;
+    @Column(name = "FIAS_ID")
+    private Integer fiasId;
+    @Column(name = "PAAS_ID")
+    private Integer passId;
+    @Column(name = "CLAY_ID")
+    private Integer clayId;
+    @Column(name = "SE_HISTORY_ID")
+    private Integer seHistoryId;
+
+}

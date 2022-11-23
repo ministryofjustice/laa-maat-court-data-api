8ab6cedfc76d7eed6d9092906a6f011653fc2b67
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2022-11-23 16:53:19 +0000

    LCAM-221|Removing the native queries as per review comments

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/IOJAppealRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/IOJAppealRepository.java
@@ -1,14 +1,16 @@
 package gov.uk.courtdata.repository;
 
 import gov.uk.courtdata.entity.IOJAppealEntity;
+import gov.uk.courtdata.entity.RepOrderMvoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IOJAppealRepository extends JpaRepository<IOJAppealEntity, Integer> {
+public interface IOJAppealRepository extends JpaRepository<IOJAppealEntity, Integer>, JpaSpecificationExecutor<IOJAppealEntity> {
 
     @Modifying
     @Query(value = "update IOJAppealEntity ioj_ae set ioj_ae.replaced = 'Y' where ioj_ae.repId = :repId and ioj_ae.id <> :iojAppealIDNotToUpdate")
@@ -17,6 +19,5 @@ public interface IOJAppealRepository extends JpaRepository<IOJAppealEntity, Inte
     @Query(value = "SELECT ioj_ae FROM IOJAppealEntity ioj_ae WHERE ioj_ae.repId = :repId AND ioj_ae.replaced = 'N'")
     IOJAppealEntity findByRepId(int repId);
 
-    @Query(value = "SELECT ioj_ae FROM IOJAppealEntity ioj_ae WHERE ioj_ae.repId = :repId AND ioj_ae.decisionResult = 'PASS' AND ioj_ae.replaced = 'N'")
-    IOJAppealEntity findCurrentPassedByRepId(int repId);
+    IOJAppealEntity findByRepIdAndReplacedAndDecisionResult(int repId, String replaced, String decisionResult);
 }

90cb6ca993bbc615425a6b4efad2a58530d9e285
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2023-02-15 09:30:19 +0000

    LASB-1672: Added template Eforms DTOs.

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/eformsApplication/dto/EformsStagingDTO.java
@@ -0,0 +1,19 @@
+package gov.uk.courtdata.eformsApplication.dto;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EformsStagingDTO implements Serializable {
+
+}

8f454f2514127a71d675ba93b9f6f167145e71a6
Alex Stone <alex.stone@digital.justice.gov.uk>
2023-04-25 15:58:25 +0100

    LASB-1832 Add implementation and test for TypeValidator

--- maat-court-data-api/src/main/java/gov/uk/courtdata/eform/validator/TypeValidator.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/eform/validator/TypeValidator.java
@@ -1,11 +1,18 @@
 package gov.uk.courtdata.eform.validator;
 
+import gov.uk.courtdata.exception.ValidationException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+/**
+ * The responsibility of this class is to validate eForms types i.e. EFORMS_STAGING.TYPE
+ */
 @Component
 public class TypeValidator {
-    
-    public void validate(String type) {
-        // TODO
+
+    public void validate(String type) throws ValidationException {
+        if (StringUtils.isBlank(type)) {
+            throw new ValidationException("Expected non-null, non-empty, non-blank type but found [" + type + "]");
+        }
     }
 }
--- maat-court-data-api/src/test/java/gov/uk/courtdata/eform/validator/TypeValidatorTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/eform/validator/TypeValidatorTest.java
@@ -1,7 +1,12 @@
 package gov.uk.courtdata.eform.validator;
 
+import gov.uk.courtdata.exception.ValidationException;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TypeValidatorTest {
 
@@ -13,7 +18,31 @@ class TypeValidatorTest {
     }
 
     @Test
-    void validate() {
-        // TODO
+    void shouldThrowValidationExceptionWhenValidatingANullType() {
+        ValidationException exception = Assertions.assertThrows(
+                ValidationException.class, () -> validator.validate(null));
+
+        assertEquals("Expected non-null, non-empty, non-blank type but found [null]", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowValidationExceptionWhenValidatingAnEmptyType() {
+        ValidationException exception = Assertions.assertThrows(
+                ValidationException.class, () -> validator.validate(StringUtils.EMPTY));
+
+        assertEquals("Expected non-null, non-empty, non-blank type but found []", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowValidationExceptionWhenValidatingABlankType() {
+        ValidationException exception = Assertions.assertThrows(
+                ValidationException.class, () -> validator.validate("      "));
+
+        assertEquals("Expected non-null, non-empty, non-blank type but found [      ]", exception.getMessage());
+    }
+
+    @Test
+    void shouldNotThrowValidationExceptionWhenValidatingAValidType() {
+        validator.validate("CRM14");
     }
 }

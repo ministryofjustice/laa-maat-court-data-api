7c6cfc9c70e4982e97f81f6b477cf19a4b97444e
Kevin-Paxton <kevin.paxton@digital.justice.gov.uk>
2023-04-25 12:56:25 +0100

    filter

--- maat-court-data-api/src/main/java/gov/uk/courtdata/filters/security/ValidationFilter.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/filters/security/ValidationFilter.java
@@ -1,4 +1,32 @@
 package gov.uk.courtdata.filters.security;
 
-public class ValidationFilter {
+import gov.uk.courtdata.filters.logging.TransactionFilter;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class ValidationFilter implements Filter {
+
+    private final static Logger LOG = LoggerFactory.getLogger(TransactionFilter.class);
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        LOG.info("Initializing filter :{}", this);
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        LOG.info("Starting Transaction for req :{}", req.getRequestURI());
+        chain.doFilter(request, response);
+        LOG.info("Committing Transaction for req :{}", req.getRequestURI());
+    }
+
+    @Override
+    public void destroy() {
+        LOG.warn("Destructing filter :{}", this);
+    }
 }

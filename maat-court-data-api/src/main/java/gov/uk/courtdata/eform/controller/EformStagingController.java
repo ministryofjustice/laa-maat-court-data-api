a227812b7e543569ad9dfd3168f0d85cdf5f221b
Alex Stone <alex.stone@digital.justice.gov.uk>
2023-05-17 16:36:03 +0100

    LASB-1972 Reduce duplication of @ApiResponse in EformStagingController

--- maat-court-data-api/src/main/java/gov/uk/courtdata/eform/controller/EformStagingController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/eform/controller/EformStagingController.java
@@ -1,7 +1,6 @@
 package gov.uk.courtdata.eform.controller;
 
 import com.amazonaws.xray.spring.aop.XRayEnabled;
-import gov.uk.courtdata.common.dto.ErrorDTO;
 import gov.uk.courtdata.eform.dto.EformStagingDTO;
 import gov.uk.courtdata.eform.mapper.EformStagingDTOMapper;
 import gov.uk.courtdata.eform.model.EformStagingResponse;
@@ -9,9 +8,6 @@ import gov.uk.courtdata.eform.service.EformStagingService;
 import gov.uk.courtdata.eform.validator.UsnValidator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -33,9 +29,7 @@ public class EformStagingController {
 
     @GetMapping(value ="/{usn}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(description = "Retrieve an EFORMS_STAGING record")
-    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
-    @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
-    @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
+    @StandardApiResponseCodes
     public ResponseEntity<EformStagingResponse> getEformApplication(@PathVariable Integer usn,
                                                                     @Parameter(description = "Used for tracing calls")
                                                                     @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
@@ -50,9 +44,7 @@ public class EformStagingController {
 
     @DeleteMapping(value ="/{usn}")
     @Operation(description = "Delete an EFORMS_STAGING record")
-    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
-    @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
-    @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
+    @StandardApiResponseCodes
     public ResponseEntity<Void> deleteEformApplication(@PathVariable Integer usn,
                                                        @Parameter(description = "Used for tracing calls")
                                                        @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
@@ -66,9 +58,7 @@ public class EformStagingController {
 
     @PostMapping(value ="/{usn}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(description = "Create an EFORMS_STAGING record")
-    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
-    @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
-    @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
+    @StandardApiResponseCodes
     public ResponseEntity<Void> createEformApplication(@PathVariable Integer usn,
                                                        @RequestParam(name = "type", required = false, defaultValue = DEFAULT_EFORM_TYPE) String type,
                                                        @RequestParam(name = "maatRef", required = false) Integer maatRef,
@@ -88,12 +78,10 @@ public class EformStagingController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping(value ="/initialise/{usn}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/initialise/{usn}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(description = "Create an EFORMS_STAGING record for Crime Apply")
-    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
-    @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
-    @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
-    public ResponseEntity<EformStagingResponse> retriveOrInsertDummyUsnRecord(@PathVariable Integer usn) {
+    @StandardApiResponseCodes
+    public ResponseEntity<EformStagingResponse> retrieveOrInsertDummyUsnRecord(@PathVariable Integer usn) {
 
         EformStagingDTO eformStagingDto = eformStagingService.createOrRetrieve(usn);
 

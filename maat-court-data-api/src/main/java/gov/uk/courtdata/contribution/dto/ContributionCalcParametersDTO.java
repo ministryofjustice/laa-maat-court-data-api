a2844d1d7f6cabf36b2cad8759c9f8e91d8ba097
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2023-05-19 09:20:19 +0100

    LCAM-393|End point to get Contribution Calc Parameters

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/contribution/dto/ContributionCalcParametersDTO.java
@@ -0,0 +1,40 @@
+package gov.uk.courtdata.contribution.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContributionCalcParametersDTO {
+
+    @NotBlank
+    private LocalDateTime fromDate;
+
+    private LocalDateTime toDate;
+
+    private BigDecimal disposableIncomePercent;
+
+    private BigDecimal upliftedIncomePercent;
+
+    private Integer totalMonths;
+
+    private Integer upfrontTotalMonths;
+
+    private BigDecimal minUpliftedMonthlyAmount;
+
+    private BigDecimal interestRate;
+
+    private Integer firstReminderDaysDue;
+
+    private Integer secondReminderDaysDue;
+
+    private BigDecimal minimumMonthlyAmount;
+}
---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/contribution/controller/ContributionCalcControllerTest.java
@@ -0,0 +1,49 @@
+package gov.uk.courtdata.contribution.controller;
+
+import gov.uk.courtdata.builder.TestModelDataBuilder;
+import gov.uk.courtdata.contribution.dto.ContributionCalcParametersDTO;
+import gov.uk.courtdata.contribution.service.ContributionCalcService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static gov.uk.courtdata.builder.TestModelDataBuilder.EFFECTIVE_DATE;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ContributionCalcController.class)
+class ContributionCalcControllerTest {
+
+    private static final String ENDPOINT_URL = "/api/internal/v1/assessment/contribution-calc-params/";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ContributionCalcService contributionCalcService;
+
+    @Test
+    void givenValidEffectiveDate_whenGetContributionCalcParametersIsInvoked_thenContributionCalcParametersDTOIsReturned() throws Exception {
+        ContributionCalcParametersDTO expected = TestModelDataBuilder.getContributionCalcParametersDTO();
+        when(contributionCalcService.getContributionCalcParameters(EFFECTIVE_DATE)).thenReturn(expected);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL + EFFECTIVE_DATE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.totalMonths").value(expected.getTotalMonths()));
+    }
+
+    @Test
+    void givenInvalidEffectiveDate_whenGetContributionCalcParametersIsInvoked_thenNotFoundIsThrown() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL))
+                .andExpect(status().isNotFound());
+    }
+
+}

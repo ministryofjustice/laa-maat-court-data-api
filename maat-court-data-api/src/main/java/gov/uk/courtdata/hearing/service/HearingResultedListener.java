939c43a1be759f7ca1182cb26814bceace6e3b0a
athar.majeed <athar.majeed@digital.justice.gov.uk>
2023-05-22 12:17:11 +0100

    WIp - upgrading the sqs listeners.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/hearing/service/HearingResultedListener.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/hearing/service/HearingResultedListener.java
@@ -4,12 +4,16 @@ import com.amazonaws.xray.spring.aop.XRayEnabled;
 import com.google.gson.Gson;
 import gov.uk.courtdata.enums.LoggingData;
 import gov.uk.courtdata.enums.MessageType;
+import gov.uk.courtdata.exception.ValidationException;
 import gov.uk.courtdata.model.hearing.HearingResulted;
 import gov.uk.courtdata.service.QueueMessageLogService;
+import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
+import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
-import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -25,12 +29,22 @@ public class HearingResultedListener {
     private final HearingResultedService hearingResultedService;
     private final QueueMessageLogService queueMessageLogService;
 
-    @JmsListener(destination = "${cloud-platform.aws.sqs.queue.hearingResulted}", concurrency = "1")
-    public void receive(@Payload final String message) {
-        MDC.put(LoggingData.REQUEST_TYPE.getValue(), MessageType.HEARING.name());
-        queueMessageLogService.createLog(MessageType.HEARING, message);
-        HearingResulted hearingResulted = gson.fromJson(message, HearingResulted.class);
-        if (hearingResulted.getHearingId() == null) hearingResulted.setHearingId(UUID.randomUUID());
-        hearingResultedService.execute(hearingResulted);
+    @SqsListener(value = "${cloud-platform.aws.sqs.queue.hearingResulted}",
+            deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void receive(@Payload final String message,
+                        final @Headers MessageHeaders headers) {
+        try {
+            log.debug("message-id {}", headers.get("MessageId"));
+            MDC.put(LoggingData.REQUEST_TYPE.getValue(), MessageType.HEARING.name());
+            queueMessageLogService.createLog(MessageType.HEARING, message);
+
+            HearingResulted hearingResulted = gson.fromJson(message, HearingResulted.class);
+
+            if (hearingResulted.getHearingId() == null) hearingResulted.setHearingId(UUID.randomUUID());
+            hearingResultedService.execute(hearingResulted);
+
+        } catch (ValidationException exception) {
+            log.warn(exception.getMessage());
+        }
     }
 }
--- maat-court-data-api/src/test/java/gov/uk/courtdata/integration/hearing/HearingResultedListenerIntegrationTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/integration/hearing/HearingResultedListenerIntegrationTest.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
@@ -30,6 +31,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
@@ -110,7 +112,7 @@ public class HearingResultedListenerIntegrationTest {
     public void givenAHearingMessageWithNoMaatId_whenMessageIsReceived_thenErrorIsReturned() {
         String payloadMissingMaatId = String.format("{\"laaTransactionId\":\"%s\"}", LAA_TRANSACTION_ID);
         ValidationException error = Assert.assertThrows(
-                ValidationException.class, () -> hearingResultedListener.receive(payloadMissingMaatId));
+                ValidationException.class, () -> hearingResultedListener.receive(payloadMissingMaatId, new MessageHeaders(new HashMap<>())));
 
         Assert.assertEquals("MAAT ID is required.", error.getMessage());
         queueMessageLogTestHelper.assertQueueMessageLogged(
@@ -122,6 +124,19 @@ public class HearingResultedListenerIntegrationTest {
                 .maatId(0)
                 .laaTransactionId(UUID.fromString(LAA_TRANSACTION_ID))
                 .jurisdictionType(JurisdictionType.CROWN).build();
+
+        runValidationErrorScenario(testData, "MAAT ID is required.");
+    }
+
+    @Test
+    public void givenAHearingMessageWithAZeroMaatId_whenMessageIsReceived_thenErrorIsReturned1() throws JsonProcessingException {
+        HearingResulted testData = HearingResulted.builder()
+                .maatId(0)
+                .functionType(FunctionType.OFFENCE)
+                .laaTransactionId(UUID.fromString(LAA_TRANSACTION_ID))
+                .jurisdictionType(JurisdictionType.CROWN).build();
+        String messageBlob = objectMapper.writeValueAsString(testData);
+        hearingResultedListener.receive(messageBlob, new MessageHeaders(new HashMap<>()));
 
         runValidationErrorScenario(testData, "MAAT ID is required.");
     }
@@ -278,7 +293,7 @@ public class HearingResultedListenerIntegrationTest {
                         WqLinkRegisterEntity.builder().createdTxId(1).maatId(TEST_MAAT_ID).caseId(TEST_CASE_ID).proceedingId(1).build());
 
         String messageBlob = objectMapper.writeValueAsString(testData);
-        hearingResultedListener.receive(messageBlob);
+        hearingResultedListener.receive(messageBlob, new MessageHeaders(new HashMap<>()));
 
         if (testData.getFunctionType() == FunctionType.APPLICATION) {
             modifyTestDataForCourtPreProcessing(testData);
@@ -565,7 +580,7 @@ public class HearingResultedListenerIntegrationTest {
 
     private <T extends Exception> void runErrorScenario(Class<T> exceptionClass, HearingResulted testPayload, String expectedErrorMessage) throws JsonProcessingException {
         String messageBlob = objectMapper.writeValueAsString(testPayload);
-        T error = Assert.assertThrows(exceptionClass, () -> hearingResultedListener.receive(messageBlob));
+        T error = Assert.assertThrows(exceptionClass, () -> hearingResultedListener.receive(messageBlob, new MessageHeaders(new HashMap<>())));
         Assert.assertEquals(error.getMessage(), expectedErrorMessage);
         queueMessageLogTestHelper.assertQueueMessageLogged(
                 messageBlob, 1, testPayload.getLaaTransactionId().toString(), testPayload.getMaatId());

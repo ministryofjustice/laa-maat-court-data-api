fadafcbd44549226d415d9ade953609865412f9f
athar.majeed <athar.majeed@digital.justice.gov.uk>
2023-08-30 18:06:47 +0100

    Updating the Query to get the single day files. DCES-112

--- maat-court-data-api/src/main/java/gov/uk/courtdata/dces/service/DebtCollectionRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/dces/service/DebtCollectionRepository.java
@@ -14,7 +14,7 @@ public class DebtCollectionRepository {
     List<String> getContributionFiles(final String fromDate, final String toDate) {
         String query = "SELECT cf.xml_content FROM TOGDATA.CONTRIBUTION_FILES CF " +
                 "WHERE CF.FILE_NAME LIKE '%%CONTRIBUTIONS%%' " +
-                "AND CF.DATE_CREATED BETWEEN TO_DATE(?, 'dd/mm/yyyy') AND TO_DATE(?, 'dd/mm/yyyy')";
+                "AND TO_DATE(CF.DATE_CREATED) BETWEEN TO_DATE(?, 'dd/mm/yyyy') AND TO_DATE(?, 'dd/mm/yyyy')";
 
         return jdbcTemplate.queryForList(query, String.class, fromDate, toDate);
     }

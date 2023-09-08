71aca352d36ba131a08f9126b7df1ce39b2143d6
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2023-09-08 15:51:15 +0100

    [LASB-2293] added SQL script

--- maat-court-data-api/src/main/resources/db/changelog/changes/increase_column_various_mla_tables.sql
+++ maat-court-data-api/src/main/resources/db/changelog/changes/increase_column_various_mla_tables.sql
@@ -22,3 +22,11 @@ ALTER TABLE "MLA"."XXMLA_WQ_DEFENDANT" modify ADDRESS_LINE1 varchar2(35 char);
 ALTER TABLE "MLA"."XXMLA_WQ_DEFENDANT" modify ADDRESS_LINE2 varchar2(35 char);
 ALTER TABLE "MLA"."XXMLA_OFFENCE" modify OFFENCE_WORDING varchar2(2500 char);
 ALTER TABLE "MLA"."XXMLA_DEFENDANT" modify SURNAME varchar2(50 char)
+
+-- LASB-2294 - SQL Queries executed on 8th Aug 2023
+
+ALTER TABLE "MLA"."XXMLA_RESULT" modify RESULT_TEXT varchar2(4000 char);
+
+--The following script is to roll back the changes in case of any problem :
+
+ALTER TABLE "MLA"."XXMLA_RESULT" modify RESULT_TEXT varchar2(2500 char);

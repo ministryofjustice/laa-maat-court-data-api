d0204f2999cbbcf3b258450833e6e445e087ba22
Alex Stone <alex.stone@digital.justice.gov.uk>
2023-09-28 14:29:10 +0100

    LASB-1982 Add new test data teardown utility class RepositoryUtil.clearUp

---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/util/RepositoryUtil.java
@@ -0,0 +1,25 @@
+package gov.uk.courtdata.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+@UtilityClass
+public class RepositoryUtil {
+
+    private static final Consumer<JpaRepository<?, ?>> DELETE_ALL_AND_FLUSH = repository -> {
+        repository.deleteAll();
+        repository.flush();
+    };
+
+    /**
+     * The responsibility of this method is to delete all and flush all entities within all the supplied repositories.
+     *
+     * @param repositories The repositories to be cleared
+     */
+    public void clearUp(JpaRepository<?, ?>... repositories) {
+        Arrays.stream(repositories).forEach(DELETE_ALL_AND_FLUSH);
+    }
+}

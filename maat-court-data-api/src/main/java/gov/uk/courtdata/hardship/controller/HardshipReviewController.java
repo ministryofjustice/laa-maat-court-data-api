774e772017a6183b069a83791c0db37ea0037bee
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2023-10-26 14:29:08 +0100

    LCAM-1002|Remove laaTransactionId from Hardship endpoints

--- maat-court-data-api/src/main/java/gov/uk/courtdata/hardship/controller/HardshipReviewController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/hardship/controller/HardshipReviewController.java
@@ -16,14 +16,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static gov.uk.courtdata.enums.LoggingData.LAA_TRANSACTION_ID;
 
 @RestController
 @RequestMapping("${api-endpoints.assessments-domain}/hardship")
@@ -46,13 +43,8 @@ public class HardshipReviewController {
             )
     )
     public ResponseEntity<HardshipReviewDTO> getHardship(
-            @PathVariable int hardshipId,
-            @Parameter(description = "Used for tracing calls")
-            @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
-
-        MDC.put(LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+            @PathVariable int hardshipId) {
         log.info("Get Hardship Review Request Received");
-
         validationProcessor.validate(hardshipId);
         return ResponseEntity.ok(hardshipReviewService.find(hardshipId));
     }
@@ -67,11 +59,8 @@ public class HardshipReviewController {
             )
     )
     public ResponseEntity<HardshipReviewDTO> getHardshipByRepId(
-            @PathVariable int repId,
-            @Parameter(description = "Used for tracing calls")
-            @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
+            @PathVariable int repId) {
 
-        MDC.put(LAA_TRANSACTION_ID.getValue(), laaTransactionId);
         log.info("Get Hardship Review by repId = {}", repId);
         return ResponseEntity.ok(hardshipReviewService.findByRepId(repId));
     }
@@ -87,11 +76,7 @@ public class HardshipReviewController {
     )
     public ResponseEntity<List<HardshipReviewDetail>> getHardshipByDetailType(
             @PathVariable int repId,
-            @PathVariable String detailType,
-            @Parameter(description = "Used for tracing calls")
-            @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
-
-        MDC.put(LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+            @PathVariable String detailType) {
         log.info("Get Hardship Review by detail type = {} and repId = {}", detailType, repId);
         return ResponseEntity.ok(hardshipReviewService.findDetails(detailType, repId));
     }
@@ -105,13 +90,8 @@ public class HardshipReviewController {
                             schema = @Schema(implementation = CreateHardshipReview.class)
                     )
             )
-            @RequestBody CreateHardshipReview hardshipReview,
-            @Parameter(description = "Used for tracing calls")
-            @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
-
-        MDC.put(LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+            @RequestBody CreateHardshipReview hardshipReview) {
         log.info("Create Hardship Review Request Received");
-
         validationProcessor.validate(hardshipReview);
         return ResponseEntity.ok(hardshipReviewService.create(hardshipReview));
     }
@@ -125,13 +105,8 @@ public class HardshipReviewController {
                             schema = @Schema(implementation = UpdateHardshipReview.class)
                     )
             )
-            @RequestBody UpdateHardshipReview hardshipReview,
-            @Parameter(description = "Used for tracing calls")
-            @RequestHeader(value = "Laa-Transaction-Id", required = false) String laaTransactionId) {
-
-        MDC.put(LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+            @RequestBody UpdateHardshipReview hardshipReview) {
         log.info("Update Hardship Review Request Received");
-
         validationProcessor.validate(hardshipReview);
         return ResponseEntity.ok(hardshipReviewService.update(hardshipReview));
     }

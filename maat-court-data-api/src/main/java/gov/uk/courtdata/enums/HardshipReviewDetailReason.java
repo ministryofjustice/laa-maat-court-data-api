a945cbaeb2f9a4ba562b5fd842204bc37fdebbe6
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-10-26 12:35:45 +0100

    removed type and added missing value to  hardship detail reason enum

--- maat-court-data-api/src/main/java/gov/uk/courtdata/enums/HardshipReviewDetailReason.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/enums/HardshipReviewDetailReason.java
@@ -12,23 +12,21 @@ import java.util.stream.Stream;
 @AllArgsConstructor
 public enum HardshipReviewDetailReason {
 
-    EVIDENCE_SUPPLIED("Evidence Supplied", HardshipReviewDetailType.EXPENDITURE.getType()),
-    ESSENTIAL_NEED_FOR_WORK("Essential - need for work", HardshipReviewDetailType.EXPENDITURE.getType()),
-    ESSENTIAL_ITEM("Essential Item", HardshipReviewDetailType.EXPENDITURE.getType()),
-    ARRANGEMENT_IN_PLACE("Arrangement in place", HardshipReviewDetailType.EXPENDITURE.getType()),
-    NO_EVIDENCE_SUPPLIED("No evidence supplied", HardshipReviewDetailType.EXPENDITURE.getType()),
-    INSUFFICIENT_EVIDENCE_SUPPLIED("Insufficient evidence supplied", HardshipReviewDetailType.EXPENDITURE.getType()),
-    NON_ESSENTIAL_ITEM_EXPENSE("Non-essential item/expense", HardshipReviewDetailType.EXPENDITURE.getType()),
-    COVERED_BY_LIVING_EXPENSE("Covered by living expense", HardshipReviewDetailType.EXPENDITURE.getType()),
-    NOT_ALLOWABLE_DIFF_FROM_NON_ESSENTIAL("Not allowable (diff from non-essential)",
-                                          HardshipReviewDetailType.EXPENDITURE.getType()
-    ),
-    NOT_IN_COMPUTATION_PERIOD("Not in computation period", HardshipReviewDetailType.EXPENDITURE.getType());
+    EVIDENCE_SUPPLIED("Evidence Supplied"),
+    ALLOWABLE_EXPENSE("Allowable Expense"),
+    ESSENTIAL_NEED_FOR_WORK("Essential - need for work"),
+    ESSENTIAL_ITEM("Essential Item"),
+    ARRANGEMENT_IN_PLACE("Arrangement in place"),
+    NO_EVIDENCE_SUPPLIED("No evidence supplied"),
+    INSUFFICIENT_EVIDENCE_SUPPLIED("Insufficient evidence supplied"),
+    NON_ESSENTIAL_ITEM_EXPENSE("Non-essential item/expense"),
+    COVERED_BY_LIVING_EXPENSE("Covered by living expense"),
+    NOT_ALLOWABLE_DIFF_FROM_NON_ESSENTIAL("Not allowable (diff from non-essential)"),
+    NOT_IN_COMPUTATION_PERIOD("Not in computation period");
 
     @JsonValue
     @JsonPropertyDescription("Hardship review detail reasons")
     private final String reason;
-    private final String type;
 
     public static HardshipReviewDetailReason getFrom(String reason) {
         if (StringUtils.isBlank(reason)) {

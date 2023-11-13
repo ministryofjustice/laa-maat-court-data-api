100d02a8d3d0b14b52f34cfe2ff1d2d0c0f8e99d
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-11-13 21:51:44 +0000

    LCAM-1057: Updated ApplicantHistoryEntry and code refactoring

--- maat-court-data-api/src/main/java/gov/uk/courtdata/preupdatechecks/repository/RepOrderApplicantLinksRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/preupdatechecks/repository/RepOrderApplicantLinksRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface RepOrderApplicantLinksRepository extends JpaRepository<RepOrderApplicantLinksEntity, Integer> {
+
     List<RepOrderApplicantLinksEntity> findAllByRepId(Integer repId);
 
 }

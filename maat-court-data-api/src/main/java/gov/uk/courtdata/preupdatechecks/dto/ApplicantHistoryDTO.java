ad8d937c501bc30e4570283e6828750f0758c8b9
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2023-11-14 09:04:48 +0000

    LCAM-1057: Updated ApplicantHistoryEntity and ApplicantHistoryDTO

--- maat-court-data-api/src/main/java/gov/uk/courtdata/preupdatechecks/dto/ApplicantHistoryDTO.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/preupdatechecks/dto/ApplicantHistoryDTO.java
@@ -1,5 +1,6 @@
 package gov.uk.courtdata.preupdatechecks.dto;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,19 +27,24 @@ public class ApplicantHistoryDTO {
     private String niNumber;
     private String foreignId;
     private String noFixedAbode;
+    private Integer homeAddrId;
+    private String emstCode;
+    private String ethnId;
+    private String disabled;
     private String phoneHome;
     private String phoneMobile;
     private String email;
     private String sortCode;
-    private String bankAccountNo;
+    private Integer bankAccountNo;
     private String bankAccountName;
-    private String sendToCclf;
     private LocalDateTime dateCreated;
     private String userCreated;
     private LocalDateTime dateModified;
     private String userModified;
+    private String pamePaymentMethod;
     private String prefPaymentDay;
-    private LocalDateTime specialInvestigation;
+    private String sendToCclf;
+    private LocalDate specialInvestigation;
     private String phoneWork;
 
 }

829c26e1c504567cc3530a09686f621b46ac3747
muthusubra <muthu.subramanian@digital.justice.gov.uk>
2023-11-15 10:19:54 +0000

    Initial Commit

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/dto/UserSummaryDTO.java
@@ -0,0 +1,20 @@
+package gov.uk.courtdata.dto;
+
+import gov.uk.courtdata.entity.ReservationsEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSummaryDTO {
+    private String userName;
+    private List<String> newWorkReasons;
+    private List<String> roleActions;
+    private ReservationsEntity reservationsEntity;
+}
---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/users/mapper/UserSummaryMapper.java
@@ -0,0 +1,23 @@
+package gov.uk.courtdata.users.mapper;
+
+import gov.uk.courtdata.dto.UserSummaryDTO;
+import gov.uk.courtdata.entity.ReservationsEntity;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+        builder = @Builder(disableBuilder = true)
+)
+public interface UserSummaryMapper {
+
+    UserSummaryDTO userToUserSummaryDTO(final String userName,
+                                               final List<String> newWorkReasons,
+                                               final List<String> roleActions,
+                                               final ReservationsEntity reservationsEntity
+    );
+}

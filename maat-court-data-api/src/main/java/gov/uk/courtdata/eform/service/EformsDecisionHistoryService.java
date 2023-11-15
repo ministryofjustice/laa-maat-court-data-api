4ab2b8b165fce6d506c05672b031be23df3d3274
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2023-11-15 09:38:40 +0000

    [LASB-2681] CRUD functionality on the eforms_decision_history table

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/eform/service/EformsDecisionHistoryService.java
@@ -0,0 +1,56 @@
+package gov.uk.courtdata.eform.service;
+
+import gov.uk.courtdata.eform.repository.EformsDecisionHistoryRepository;
+import gov.uk.courtdata.eform.repository.entity.EformsDecisionHistory;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EformsDecisionHistoryService {
+
+    private final EformsDecisionHistoryRepository eformsDecisionHistoryRepository;
+
+    @Transactional
+    public List<EformsDecisionHistory> getAllEformsDecisionHistory(Integer usn) {
+        return eformsDecisionHistoryRepository.findAllByUsn(usn);
+    }
+    @Transactional
+    public EformsDecisionHistory getNewEformsDecisionHistoryRecord(Integer usn) {
+        return eformsDecisionHistoryRepository.findTopByUsnOrderByIdDesc(usn);
+    }
+
+    @Transactional
+    public EformsDecisionHistory getPreviousEformsDecisionHistoryRecordWroteToResult(Integer usn) {
+        return eformsDecisionHistoryRepository.findFirstByUsnAndWroteToResultsOrderByIdDesc(usn, "Y");
+    }
+
+   public void createEformsDecisionHistory(EformsDecisionHistory eformsDecisionHistory) {
+        eformsDecisionHistoryRepository.save(eformsDecisionHistory);
+    }
+
+    public void deleteEformsDecisionHistory(Integer usn) {
+        eformsDecisionHistoryRepository.deleteAllByUsn(usn);
+    }
+
+   public void updateEformsDecisionHistoryFields(Integer usn, Map<String, Object> updateFields) {
+
+        Optional<EformsDecisionHistory> latestEformsDecisionHistory = Optional.ofNullable(eformsDecisionHistoryRepository.findTopByUsnOrderByIdDesc(usn));
+
+        if (latestEformsDecisionHistory.isPresent()) {
+            updateFields.forEach((key, value) -> {
+                Field field = ReflectionUtils.findField(EformsDecisionHistory.class, key);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, latestEformsDecisionHistory.get(), value);
+            });
+            eformsDecisionHistoryRepository.save(latestEformsDecisionHistory.get());
+        }
+    }
+}

c72be5ee6cc5bcb430bb555ab397b0f22c76a9a2
athar.majeed <athar.majeed@digital.justice.gov.uk>
2023-12-01 15:32:17 +0000

    Fixing broken test on maat-api

--- maat-court-data-api/src/main/java/gov/uk/courtdata/contribution/controller/ContributionsController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/contribution/controller/ContributionsController.java
@@ -28,6 +28,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+///api/internal/v1/assessment/contributions
 @Tag(name = "Contributions", description = "Rest API for contributions")
 @RequestMapping("${api-endpoints.assessments-domain}/contributions")
 public class ContributionsController {

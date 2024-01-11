5f123beaad49612943ce5c30297bbde5f4766f96
Alex Stone <alex.stone@digital.justice.gov.uk>
2024-01-11 12:15:41 +0000

    LASB-2827 Add get user by RepId native query

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/RepOrderRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/RepOrderRepository.java
@@ -3,10 +3,24 @@ package gov.uk.courtdata.repository;
 import gov.uk.courtdata.entity.RepOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface RepOrderRepository extends JpaRepository<RepOrderEntity, Integer>, JpaSpecificationExecutor<RepOrderEntity> {
 
+    @Query(value = """
+            SELECT
+                NVL(INITCAP(U.FIRST_NAME
+                            || ' '
+                            || U.SURNAME),
+                    R.USER_CREATED) User_Created
+            FROM
+                REP_ORDERS R
+                LEFT JOIN USERS U ON (U.USER_NAME = R.USER_CREATED)
+            WHERE
+                ID = :repOrderId;
+                """, nativeQuery = true)
+    Object findRepOrderCreator(Integer repOrderId);
 }

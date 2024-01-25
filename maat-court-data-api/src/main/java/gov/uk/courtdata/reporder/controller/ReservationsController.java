1184c15f5d0707832fd3e32d2846a1a4e4277627
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2024-01-25 22:00:33 +0000

    Refactor and merge Reservations Controllers

--- maat-court-data-api/src/main/java/gov/uk/courtdata/reporder/controller/ReservationsController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/reporder/controller/ReservationsController.java
@@ -2,27 +2,84 @@ package gov.uk.courtdata.reporder.controller;
 
 import gov.uk.courtdata.annotation.NotFoundApiResponse;
 import gov.uk.courtdata.annotation.StandardApiResponse;
+import gov.uk.courtdata.constants.CourtDataConstants;
 import gov.uk.courtdata.entity.ReservationsEntity;
+import gov.uk.courtdata.enums.LoggingData;
+import gov.uk.courtdata.model.authorization.AuthorizationResponse;
+import gov.uk.courtdata.prosecutionconcluded.helper.ReservationsRepositoryHelper;
 import gov.uk.courtdata.reporder.service.ReservationsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uk.gov.justice.laa.crime.commons.common.Constants;
+
+import java.util.Optional;
+
+import static gov.uk.courtdata.enums.LoggingData.LAA_TRANSACTION_ID;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Reservations", description = "Rest API for Reservations")
-@RequestMapping("/api/internal/v1/rep-orders/reservations")
+@RequestMapping("/api/internal/v1")
 public class ReservationsController {
     private final ReservationsService reservationsService;
 
-    @GetMapping(value = "/{id}")
+    private final ReservationsRepositoryHelper reservationsRepositoryHelper;
+
+    @GetMapping(value = "/assessment/reservations/{maatId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Check if a given maatId is Locked through Reservations table")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @NotFoundApiResponse
+    @StandardApiResponse
+    public ResponseEntity<Boolean> isMaatRecordLocked(
+            @PathVariable int maatId,
+            @Parameter(description = "Used for tracing calls")
+            @RequestHeader(value = CourtDataConstants.LAA_TRANSACTION_ID, required = false) String laaTransactionId) {
+        MDC.put(LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+        log.info(String.format("Check if maatId is locked - %d {}", maatId));
+        return ResponseEntity.ok(reservationsRepositoryHelper.isMaatRecordLocked(maatId));
+    }
+
+    @GetMapping(value = "/assessment/reservations/recordname/{recordName}/recordid/{recordId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Get the reservation status object of a record")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AuthorizationResponse.class)))
+    @StandardApiResponse
+    public ResponseEntity<ReservationsEntity> getReservationByRecordNameAndRecordId(
+            @PathVariable String recordName, @PathVariable Integer recordId,
+            @Parameter(description = "Used for tracing calls")
+            @RequestHeader(value = Constants.LAA_TRANSACTION_ID, required = false) String laaTransactionId) {
+        MDC.put(LoggingData.LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+        log.info("Check reservation status - request received");
+        Optional<ReservationsEntity> reservationsEntity = reservationsRepositoryHelper.getReservationByRecordNameAndRecordId(recordName,recordId);
+        return reservationsEntity.map(c -> ResponseEntity.ok().body(c))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(value = "/assessment/reservations/username/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Get the reservation status object of a record")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AuthorizationResponse.class)))
+    @StandardApiResponse
+    public ResponseEntity<ReservationsEntity> getReservationByUsername(
+            @PathVariable String username,
+            @RequestHeader(value = Constants.LAA_TRANSACTION_ID, required = false) String laaTransactionId) {
+        MDC.put(LoggingData.LAA_TRANSACTION_ID.getValue(), laaTransactionId);
+        log.info("Check reservation status - request received");
+        Optional<ReservationsEntity> reservationsEntity = reservationsRepositoryHelper.getReservationByUserName(username);
+        return reservationsEntity.map(c -> ResponseEntity.ok().body(c))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(value = "/rep-orders/reservations/{id}")
     @Operation(description = "Retrieve a reservation")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
     @StandardApiResponse
@@ -31,7 +88,7 @@ public class ReservationsController {
         return reservationsService.retrieve(id);
     }
 
-    @PostMapping
+    @PostMapping(value = "/rep-orders/reservations")
     @Operation(description = "Create a Reservation")
     @StandardApiResponse
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
@@ -40,7 +97,7 @@ public class ReservationsController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/rep-orders/reservations/{id}")
     @Operation(description = "Update a Reservation")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
     @StandardApiResponse
@@ -50,7 +107,7 @@ public class ReservationsController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/rep-orders/reservations/{id}")
     @Operation(description = "Delete a Reservation")
     @StandardApiResponse
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
--- maat-court-data-api/src/test/java/gov/uk/courtdata/reporder/controller/ReservationsControllerTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/reporder/controller/ReservationsControllerTest.java
@@ -1,8 +1,10 @@
 package gov.uk.courtdata.reporder.controller;
 
 import gov.uk.courtdata.builder.TestModelDataBuilder;
+import gov.uk.courtdata.constants.CourtDataConstants;
 import gov.uk.courtdata.entity.ReservationsEntity;
 import gov.uk.courtdata.exception.RequestedObjectNotFoundException;
+import gov.uk.courtdata.prosecutionconcluded.helper.ReservationsRepositoryHelper;
 import gov.uk.courtdata.reporder.service.ReservationsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,19 +15,27 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import static gov.uk.courtdata.builder.TestModelDataBuilder.RESERVATION_ID;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(ReservationsController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class ReservationsControllerTest {
-    private static final String END_POINT_URL = "/api/internal/v1/rep-orders/reservations";
+    private static final String ENDPOINT_URL = "/api/internal/v1/assessment/reservations/";
+    private static final String BASE_END_POINT_URL = "/api/internal/v1/rep-orders/reservations";
+    private static final String END_POINT_URL = BASE_END_POINT_URL + "/{id}";
     private static final int NON_EXISTENT_ID = 1234;
     private static final ReservationsEntity RESERVATIONS_ENTITY = ReservationsEntity.builder()
             .recordId(RESERVATION_ID)
             .build();
+
+    @MockBean
+    private ReservationsRepositoryHelper reservationsRepositoryHelper;
 
     @MockBean
     ReservationsService reservationsService;
@@ -37,7 +47,7 @@ public class ReservationsControllerTest {
         when(reservationsService.retrieve(RESERVATION_ID))
                 .thenReturn(RESERVATIONS_ENTITY);
 
-        mvc.perform(MockMvcRequestBuilders.get(END_POINT_URL + "/" + RESERVATION_ID)
+        mvc.perform(MockMvcRequestBuilders.get(END_POINT_URL, RESERVATION_ID)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"recordId\":" + RESERVATION_ID + "}"));
@@ -48,7 +58,7 @@ public class ReservationsControllerTest {
         when(reservationsService.retrieve(NON_EXISTENT_ID))
                 .thenThrow(new RequestedObjectNotFoundException("No Reservations found with Id: " + NON_EXISTENT_ID));
 
-        mvc.perform(MockMvcRequestBuilders.get(END_POINT_URL + "/" + NON_EXISTENT_ID)
+        mvc.perform(MockMvcRequestBuilders.get(END_POINT_URL, NON_EXISTENT_ID)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andExpect(content().json("{\"code\":\"NOT_FOUND\",\"message\":\"No Reservations found with Id: " + NON_EXISTENT_ID + "\"}"));
@@ -59,7 +69,7 @@ public class ReservationsControllerTest {
 
         ReservationsEntity reservationsEntity = TestModelDataBuilder.getReservationsEntity();
 
-        mvc.perform(MockMvcRequestBuilders.post(END_POINT_URL)
+        mvc.perform(MockMvcRequestBuilders.post(BASE_END_POINT_URL)
                         .content(reservationEntityJson())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -72,7 +82,7 @@ public class ReservationsControllerTest {
 
         ReservationsEntity reservationsEntity = TestModelDataBuilder.getReservationsEntity();
 
-        mvc.perform(MockMvcRequestBuilders.patch(END_POINT_URL + "/" + RESERVATION_ID)
+        mvc.perform(MockMvcRequestBuilders.put(END_POINT_URL, RESERVATION_ID)
                         .content(reservationEntityJson())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -82,7 +92,7 @@ public class ReservationsControllerTest {
 
     @Test
     void givenValidId_whenDeleteReservationIsCalled_thenReservationIsSuccessfullyDeleted() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.delete(END_POINT_URL + "/" + RESERVATION_ID)
+        mvc.perform(MockMvcRequestBuilders.delete(END_POINT_URL, RESERVATION_ID)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
@@ -90,13 +100,79 @@ public class ReservationsControllerTest {
     }
 
     private static String reservationEntityJson() {
-        return "{\n" +
-                "\"recordId\": " + RESERVATION_ID + ",\n" +
-                "\"reservationDate\": null,\n" +
-                "\"expiryDate\": null,\n" +
-                "\"recordName\": \"mock-record\",\n" +
-                "\"userName\": \"mock-user\",\n" +
-                "\"userSession\": \"mock-session\"\n" +
-                "}";
+        return """
+                {
+                  "recordId": %d,
+                  "reservationDate": null,
+                  "expiryDate": null,
+                  "recordName": "mock-record",
+                  "userName": "mock-user",
+                  "userSession": "mock-session"
+                }
+                """.formatted(RESERVATION_ID);
+    }
+
+    @Test
+    void givenIncorrectParameters_whenIsMaatRecordLockedIsInvoked_thenErrorIsThrown() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void givenAValidParameters_whenIsMaatRecordLockedIsInvoked_thenReturnTrue() throws Exception {
+        when(reservationsRepositoryHelper.isMaatRecordLocked(TestModelDataBuilder.REP_ID))
+                .thenReturn(Boolean.TRUE);
+        mvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL + TestModelDataBuilder.REP_ID))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").value(Boolean.TRUE));
+    }
+
+    @Test
+    void givenAValidParameters_whenGetReservationByRecordNameAndRecordIdIsInvoked_thenReturnReservationsEntity() throws Exception {
+        when(reservationsRepositoryHelper.getReservationByRecordNameAndRecordId(CourtDataConstants.RESERVATION_RECORD_NAME, TestModelDataBuilder.REP_ID))
+                .thenReturn(Optional.ofNullable(ReservationsEntity.builder()
+                        .recordName(TestModelDataBuilder.RESERVATION_RECORD_NAME)
+                        .recordId(TestModelDataBuilder.REP_ID)
+                        .userSession(TestModelDataBuilder.USER_SESSION)
+                        .userName(TestModelDataBuilder.USER_NAME)
+                        .reservationDate(LocalDateTime.now())
+                        .expiryDate(LocalDateTime.now())
+                        .build()
+                ));
+        mvc.perform(MockMvcRequestBuilders.get( ENDPOINT_URL + "/recordname/" + CourtDataConstants.RESERVATION_RECORD_NAME + "/recordid/" + TestModelDataBuilder.REP_ID))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.recordId").value(TestModelDataBuilder.REP_ID));
+    }
+
+    @Test
+    void givenIncorrectParameters_whenGetReservationByRecordNameAndRecordIdIsInvoked_thenErrorIsThrown() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL + "/recordname/" + CourtDataConstants.RESERVATION_RECORD_NAME + "/recordid/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void givenAValidParameters_whenGetReservationByUserNameIsInvoked_thenReturnReservationsEntity() throws Exception {
+        when(reservationsRepositoryHelper.getReservationByUserName(TestModelDataBuilder.USER_NAME))
+                .thenReturn(Optional.ofNullable(ReservationsEntity.builder()
+                        .recordName(TestModelDataBuilder.RESERVATION_RECORD_NAME)
+                        .recordId(TestModelDataBuilder.REP_ID)
+                        .userSession(TestModelDataBuilder.USER_SESSION)
+                        .userName(TestModelDataBuilder.USER_NAME)
+                        .reservationDate(LocalDateTime.now())
+                        .expiryDate(LocalDateTime.now())
+                        .build()
+                ));
+        mvc.perform(MockMvcRequestBuilders.get( ENDPOINT_URL + "/username/" + TestModelDataBuilder.USER_NAME))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.recordId").value(TestModelDataBuilder.REP_ID));
+    }
+
+    @Test
+    void givenIncorrectParameters_whenGetReservationByUserNameIsInvoked_thenErrorIsThrown() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL + "/username/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 }

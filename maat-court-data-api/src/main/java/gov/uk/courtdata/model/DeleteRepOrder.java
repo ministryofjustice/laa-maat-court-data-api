0a708c4fb77de08eae16cd356cfe52806682b844
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2024-06-11 09:59:31 +0100

    LCAM-1358-test dup

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/model/DeleteRepOrder.java
@@ -0,0 +1,91 @@
+package gov.uk.courtdata.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteRepOrder {
+
+    @NotNull
+    private String caseId;
+    private String iojResult;
+    private String catyCaseType;
+    private String appealTypeCode;
+    private String arrestSummonsNo;
+    private String userModified;
+    @NotNull
+    private String userCreated;
+    private String magsOutcome;
+    private String magsOutcomeDate;
+    private LocalDateTime magsOutcomeDateSet;
+    private LocalDate committalDate;
+    private String decisionReasonCode;
+    private Integer crownRepId;
+    private String crownRepOrderDecision;
+    private String crownRepOrderType;
+    private LocalDate crownRepOrderDate;
+    private LocalDate crownWithdrawalDate;
+    private Boolean isImprisoned;
+    private LocalDate assessmentDateCompleted;
+    private LocalDate sentenceOrderDate;
+    private Integer applicantHistoryId;
+    private String evidenceFeeLevel;
+    private Integer bankAccountNo;
+    private String bankAccountName;
+    private String paymentMethod;
+    private Integer preferredPaymentDay;
+    private String sortCode;
+    private Boolean isSendToCCLF;
+    @NotNull
+    private Integer areaId;
+    @NotNull
+    private Integer cmuId;
+    private Boolean isCaseTransferred;
+    private Boolean isBenchWarrantIssued;
+    private LocalDate appealSentenceOrderChangedDate;
+    private LocalDate appealSentenceOrderDate;
+    private LocalDate appealReceivedDate;
+    private LocalDate appealTypeDate;
+    private LocalDate appSignedDate;
+    private Integer usn;
+    private LocalDate firstCapitalReminderDate;
+    private LocalDate allCapitalEvidenceReceivedDate;
+    private Integer applicationId;
+    private LocalDate capitalAllowanceReinstatedDate;
+    private LocalDate capitalAllowanceWithheldDate;
+    private LocalDate capitalEvidenceDueDate;
+    private String capitalNote;
+    private Integer capitalAllowance;
+    private Boolean isCourtCustody;
+    @NotNull
+    private LocalDate dateReceived;
+    private LocalDate dateStatusDue;
+    private LocalDate dateStatusSet;
+    private LocalDate decisionDate;
+    private String iojResultNote;
+    private String macoCourt;
+    private LocalDate magsWithdrawalDate;
+    private Boolean isNoCapitalDeclared;
+    private String oftyOffenceType;
+    private Boolean useSuppAddressForPost;
+    private Integer postalAddressId;
+    private String rorsStatus;
+    private String statusReason;
+    private String suppAccountCode;
+    private Boolean isWelshCorrespondence;
+    private String cinrCode;
+    private Boolean isPartner;
+    private Boolean isRetrial;
+    private LocalDate efmDateStamp;
+    private String solicitorName;
+    private LocalDate hearingDate;
+}

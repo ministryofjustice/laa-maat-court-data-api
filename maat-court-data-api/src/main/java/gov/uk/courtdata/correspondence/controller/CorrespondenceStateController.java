76981de1d7a72fb0a837696193b0887265a88446
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-07-19 11:48:43 +0100

    Use @StandardApiResponse to cut down on duplication

--- maat-court-data-api/src/main/java/gov/uk/courtdata/correspondence/controller/CorrespondenceStateController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/correspondence/controller/CorrespondenceStateController.java
@@ -1,8 +1,8 @@
 package gov.uk.courtdata.correspondence.controller;
 
 import gov.uk.courtdata.annotation.NotFoundApiResponse;
+import gov.uk.courtdata.annotation.StandardApiResponse;
 import gov.uk.courtdata.correspondence.service.CorrespondenceStateService;
-import gov.uk.courtdata.dto.ErrorDTO;
 import gov.uk.courtdata.enums.LoggingData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,13 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.justice.laa.crime.enums.contribution.CorrespondenceStatus;
 
 @Slf4j
@@ -37,12 +31,7 @@ public class CorrespondenceStateController {
     @ApiResponse(responseCode = "200",
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
     )
-    @ApiResponse(responseCode = "500",
-            description = "Server Error.",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ErrorDTO.class)
-            )
-    )
+    @StandardApiResponse
     @NotFoundApiResponse
     public ResponseEntity<CorrespondenceStatus> find(@PathVariable int repId) {
         LoggingData.MAAT_ID.putInMDC(repId);
@@ -57,18 +46,7 @@ public class CorrespondenceStateController {
                     schema = @Schema(implementation = CorrespondenceStatus.class)
             )
     )
-    @ApiResponse(responseCode = "400",
-            description = "Bad Request.",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ErrorDTO.class)
-            )
-    )
-    @ApiResponse(responseCode = "500",
-            description = "Server Error.",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ErrorDTO.class)
-            )
-    )
+    @StandardApiResponse
     public ResponseEntity<CorrespondenceStatus> create(@PathVariable int repId,
                                                          @RequestBody @Valid CorrespondenceStatus status) {
         LoggingData.MAAT_ID.putInMDC(repId);
@@ -84,18 +62,7 @@ public class CorrespondenceStateController {
                     schema = @Schema(implementation = CorrespondenceStatus.class)
             )
     )
-    @ApiResponse(responseCode = "400",
-            description = "Bad Request.",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ErrorDTO.class)
-            )
-    )
-    @ApiResponse(responseCode = "500",
-            description = "Server Error.",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ErrorDTO.class)
-            )
-    )
+    @StandardApiResponse
     public ResponseEntity<CorrespondenceStatus> update(@PathVariable int repId,
                                                          @RequestBody @Valid CorrespondenceStatus status) {
         LoggingData.MAAT_ID.putInMDC(repId);

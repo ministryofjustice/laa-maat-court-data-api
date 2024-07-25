bb32d58036e3b8d7dbc365cb98f90d5a4d6fab12
Alex Stone <alex.stone@digital.justice.gov.uk>
2024-07-25 16:14:50 +0100

    LASB-3486 Add unit tests

---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/util/SemanticVersionTest.java
@@ -0,0 +1,20 @@
+package gov.uk.courtdata.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SemanticVersionTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void asText() {
+  }
+}

8723373e55c79906dad097746149d0051e2e0718
Alex Stone <alex.stone@digital.justice.gov.uk>
2024-07-26 09:52:04 +0100

    LASB-3486 Add unit tests

---
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/util/VersionMetadataTest.java
@@ -0,0 +1,49 @@
+package gov.uk.courtdata.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class VersionMetadataTest {
+
+  @Mock
+  private SemanticVersion mockSemanticVersion;
+
+  @Mock
+  private GitProperties mockGitProperties;
+
+  private VersionMetadata versionMetadata;
+
+  @BeforeEach
+  void setUp() {
+    versionMetadata = new VersionMetadata(mockSemanticVersion, mockGitProperties);
+  }
+
+  @Test
+  void getSemanticVersion_shouldReturnSemanticVersion() {
+    final String semanticVersion = "1.0.0";
+    when(mockSemanticVersion.asText())
+        .thenReturn(semanticVersion);
+
+    String actualSemanticVersion = versionMetadata.getSemanticVersion();
+
+    assertEquals(semanticVersion, actualSemanticVersion);
+  }
+
+  @Test
+  void getGitPropertyValue_shouldReturnGitPropertyValue() {
+    final String gitCommitIdValue = "4ddf8ddc55a6348fefea65554bf8316dd4ffdb20";
+    when(mockGitProperties.getValueOf(GitProperty.GIT_COMMIT_ID))
+        .thenReturn(gitCommitIdValue);
+
+    String actualGitPropertyValue = versionMetadata.getGitPropertyValue(GitProperty.GIT_COMMIT_ID);
+
+    assertEquals(gitCommitIdValue, actualGitPropertyValue);
+  }
+}

a91cea4dc8c410d620c4e6e29383ba8525c8e387
David Conneely <899881+dconneely@users.noreply.github.com>
2024-08-05 22:27:31 +0100

    DCES-429 Improve performance of contribution file updates
    - Add specific increment of recordsReceived to Repository
    - Update Service to use Repository class instead of Entity
    - Simplify tests given simpler implementation

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/ContributionFilesRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/ContributionFilesRepository.java
@@ -2,7 +2,9 @@ package gov.uk.courtdata.repository;
 
 import gov.uk.courtdata.entity.ContributionFilesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -18,4 +20,15 @@ public interface ContributionFilesRepository extends JpaRepository<ContributionF
     @Query(value = "SELECT cf.xml_content FROM TOGDATA.CONTRIBUTION_FILES CF WHERE CF.FILE_NAME like '%%FDC%%' AND CF.DATE_CREATED BETWEEN fromDate and toDate",
             nativeQuery = true)
     List<String> getFDC(LocalDate fromDate, LocalDate toDate);
+
+    @Modifying
+    @Query("""
+           UPDATE ContributionFilesEntity cf
+           SET cf.recordsReceived = COALESCE(cf.recordsReceived, 0) + 1,
+               cf.dateReceived = CURRENT_DATE,
+               cf.dateModified = CURRENT_DATE,
+               cf.userModified = :userModified
+           WHERE cf.fileId = :id""")
+    int incrementRecordsReceived(@Param("id") Integer id,
+                                 @Param("userModified") String userModified);
 }

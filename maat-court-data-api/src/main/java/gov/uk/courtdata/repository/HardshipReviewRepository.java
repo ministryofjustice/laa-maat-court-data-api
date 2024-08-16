ae2807a9128d0b6852f87e2e6eabf568df9a7ef0
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-08-16 13:44:55 +0100

    Fixed issue where creating a new Hardship was not replacing existing records
    A new replaceOldHardshipReviews method has been introduced in the HardshipReviewRepository that is called as part of the creation logic that will mark old reviews as replaced. Without this applications may fail to load in MAAT because there are too many Hardship records returned.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/HardshipReviewRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/HardshipReviewRepository.java
@@ -15,6 +15,10 @@ public interface HardshipReviewRepository extends JpaRepository<HardshipReviewEn
     @Query(value = "UPDATE HardshipReviewEntity hr set hr.replaced = 'Y' WHERE hr.repId = :repId and hr.financialAssessmentId <> :financialAssessmentId")
     void replaceOldHardshipReviews(@Param("repId") Integer repId, @Param("financialAssessmentId") Integer financialAssessmentId);
 
+    @Modifying
+    @Query(value = "UPDATE HardshipReviewEntity hr set hr.replaced = 'Y' WHERE hr.repId = :repId and hr.courtType = :courtType")
+    void replaceOldHardshipReviews(@Param("repId") Integer repId, @Param("courtType") String courtType);
+
     @Query(value = "SELECT hr FROM HardshipReviewEntity hr WHERE hr.repId = :repId AND hr.replaced = 'N'")
     HardshipReviewEntity findByRepId(@Param("repId") Integer repId);
 }

de7875d25b56afb7392d4510e6ce2c157d0b8895
liam.murphy <liam.murphy@digital.justice.gov.uk>
2024-09-02 11:22:49 +0100

    LASB-3554 - refactored some code

--- maat-court-data-api/src/main/java/gov/uk/courtdata/reporder/mapper/RepOrderMapper.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/reporder/mapper/RepOrderMapper.java
@@ -17,8 +17,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.Comparator;
-import java.util.List;
-import java.util.function.Function;
+import java.util.Optional;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
@@ -44,8 +43,13 @@ public interface RepOrderMapper {
 
     default RepOrderStateDTO mapRepOrderState(RepOrderEntity repOrderEntity) {
         if (repOrderEntity == null) {
-            return null;
+            return RepOrderStateDTO.builder().build();
         }
+
+        Optional<FinancialAssessmentEntity> maxIdFinancialAssessmentEntity = repOrderEntity.getFinancialAssessments().stream()
+                .max(Comparator.comparingInt(FinancialAssessmentEntity::getId));
+        Optional<PassportAssessmentEntity> maxIdPassportAssessmentEntity = repOrderEntity.getPassportAssessments().stream()
+                .max(Comparator.comparingInt(PassportAssessmentEntity::getId));
 
         return RepOrderStateDTO.builder()
                 .usn(repOrderEntity.getUsn())
@@ -56,41 +60,18 @@ public interface RepOrderMapper {
                 .iojAssesorName(createIOJAssessorDetails(repOrderEntity))
                 .dateAppCreated(repOrderEntity.getDateCreated())
                 .iojReason(repOrderEntity.getIojResultNote())
-                .meansInitResult(getFinancialAssessmentDataByExpression(repOrderEntity, FinancialAssessmentEntity::getId, FinancialAssessmentEntity::getInitResult))
-                .meansInitStatus(getFinancialAssessmentDataByExpression(repOrderEntity, FinancialAssessmentEntity::getId, FinancialAssessmentEntity::getFassInitStatus))
-                .meansFullResult(getFinancialAssessmentDataByExpression(repOrderEntity, FinancialAssessmentEntity::getId, FinancialAssessmentEntity::getFullResult))
-                .meansFullStatus(getFinancialAssessmentDataByExpression(repOrderEntity, FinancialAssessmentEntity::getId, FinancialAssessmentEntity::getFassFullStatus))
-                .meansAssessorName(getFinancialAssessmentDataByExpression(repOrderEntity, FinancialAssessmentEntity::getId, financialAssessment -> UserEntityUtils.extractFullName(financialAssessment.getUserCreatedEntity())))
-                .dateMeansCreated(getFinancialAssessmentDataByExpression(repOrderEntity, FinancialAssessmentEntity::getId, FinancialAssessmentEntity::getDateCreated))
-                .passportResult(getPassportAssessmentDataByExpression(repOrderEntity, PassportAssessmentEntity::getId, PassportAssessmentEntity::getResult))
-                .passportStatus(getPassportAssessmentDataByExpression(repOrderEntity,PassportAssessmentEntity::getId, PassportAssessmentEntity::getPastStatus))
-                .passportAssessorName(getPassportAssessmentDataByExpression(repOrderEntity,PassportAssessmentEntity::getId, passportAssessmentEntity -> UserEntityUtils.extractFullName(passportAssessmentEntity.getUserCreatedEntity())))
-                .datePassportCreated(getPassportAssessmentDataByExpression(repOrderEntity,PassportAssessmentEntity::getId, PassportAssessmentEntity::getDateCreated))
+                .meansInitResult(maxIdFinancialAssessmentEntity.map(FinancialAssessmentEntity::getInitResult).orElse(null))
+                .meansInitStatus(maxIdFinancialAssessmentEntity.map(FinancialAssessmentEntity::getFassInitStatus).orElse(null))
+                .meansFullResult(maxIdFinancialAssessmentEntity.map(FinancialAssessmentEntity::getFullResult).orElse(null))
+                .meansFullStatus(maxIdFinancialAssessmentEntity.map(FinancialAssessmentEntity::getFassFullStatus).orElse(null))
+                .meansAssessorName(maxIdFinancialAssessmentEntity.map(fae -> UserEntityUtils.extractFullName(fae.getUserCreatedEntity())).orElse(null))
+                .dateMeansCreated(maxIdFinancialAssessmentEntity.map(FinancialAssessmentEntity::getDateCreated).orElse(null))
+                .passportResult(maxIdPassportAssessmentEntity.map(PassportAssessmentEntity::getResult).orElse(null))
+                .passportStatus(maxIdPassportAssessmentEntity.map(PassportAssessmentEntity::getPastStatus).orElse(null))
+                .passportAssessorName(maxIdPassportAssessmentEntity.map(pae -> UserEntityUtils.extractFullName(pae.getUserCreatedEntity())).orElse(null))
+                .datePassportCreated(maxIdPassportAssessmentEntity.map(PassportAssessmentEntity::getDateCreated).orElse(null))
                 .fundingDecision(repOrderEntity.getDecisionReasonCode())
                 .build();
-    }
-
-    private <E, T> T getAssessmentDataByExpression(List<E> assessments, Function<E, Integer> idExtractor, Function<E, T> expressionToGetDataFromAssessment) {
-        if (assessments != null && !assessments.isEmpty()) {
-
-            // Sort the assessments by ID in descending order, so we get the latest assessment
-            assessments.sort(Comparator.comparing(idExtractor).reversed());
-
-            for (E assessment : assessments) {
-                if (assessment != null) {
-                    return expressionToGetDataFromAssessment.apply(assessment);
-                }
-            }
-        }
-        return null;
-    }
-
-    private <T> T getFinancialAssessmentDataByExpression(RepOrderEntity repOrderEntity, Function<FinancialAssessmentEntity, Integer> idExtractor, Function<FinancialAssessmentEntity, T> expression) {
-        return getAssessmentDataByExpression(repOrderEntity.getFinancialAssessments(), idExtractor, expression);
-    }
-
-    private <T> T getPassportAssessmentDataByExpression(RepOrderEntity repOrderEntity, Function<PassportAssessmentEntity, Integer> idExtractor, Function<PassportAssessmentEntity, T> expression) {
-        return getAssessmentDataByExpression(repOrderEntity.getPassportAssessments(), idExtractor, expression);
     }
 
 }

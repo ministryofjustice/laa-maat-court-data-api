d2ba2a4ca73a060151cd7071d05e7fc78e906f7d
Karl Baker <karl.baker@digital.justice.gov.uk>
2024-09-26 14:42:10 +0100

    Remove secrets from docker-compose.override.yml
    This commit removes secrets from docker-compose.override.yml in favour of
    storing these in 1Password and pulling them in at runtime via the use of
    a .env file.
    
    It includes a modified version of the script created by @Mhowell494 to make
    the .env file available at the time of running the app and to ensure that
    it is cleaned up after the app has exited.

---
+++ maat-court-data-api/start-local.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Ensure the script stops if any command fails
+set -euo pipefail
+
+# Specify the vault and the document name in 1Password
+VAULT="LAA Crime Apps"
+DOCUMENT="EnvironmentVariables-MAATCourtDataApi-App"
+APP_ENV_FILE="./app.env"
+
+function remove_env_file()
+{
+    echo "Removing .env file"
+    rm -f "$APP_ENV_FILE"
+}
+
+trap remove_env_file EXIT
+
+echo "Signing into 1Password..."
+eval $(op signin --account ministryofjustice)
+
+echo "Fetching latest .env file from 1Password..."
+op document get "$DOCUMENT" --vault "$VAULT" --output "$APP_ENV_FILE" --force
+
+if [ ! -f "$APP_ENV_FILE" ]; then
+    echo "Failed to retrieve .env file from 1Password."
+    exit 1
+fi
+
+echo ".env file successfully retrieved."
+
+echo "Starting Docker containers..."
+docker-compose up --build

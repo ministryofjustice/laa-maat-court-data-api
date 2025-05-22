50ac483c2f8d258edc83f4313cef027a845ceaa4
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2025-05-22 22:59:10 +0100

    Create Resource Server Config for using Cognito OAuth

---
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/config/ResourceServerConfiguration.java
@@ -0,0 +1,49 @@
+package gov.uk.courtdata.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfiguration {
+
+    public static final String SCOPE_MAATAPI_STANDARD = "SCOPE_maatapi/standard";
+
+    @Bean
+    protected BearerTokenAuthenticationEntryPoint bearerTokenAuthenticationEntryPoint() {
+        BearerTokenAuthenticationEntryPoint bearerTokenAuthenticationEntryPoint = new BearerTokenAuthenticationEntryPoint();
+        bearerTokenAuthenticationEntryPoint.setRealmName("MAAT Data API");
+        return bearerTokenAuthenticationEntryPoint;
+    }
+
+    @Bean
+    public AccessDeniedHandler bearerTokenAccessDeniedHandler() {
+        return new BearerTokenAccessDeniedHandler();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/open-api/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/**").hasAuthority(SCOPE_MAATAPI_STANDARD)
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2ResourceServer) ->
+                        oauth2ResourceServer
+                                .accessDeniedHandler(bearerTokenAccessDeniedHandler())
+                                .authenticationEntryPoint(bearerTokenAuthenticationEntryPoint())
+                                .jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}

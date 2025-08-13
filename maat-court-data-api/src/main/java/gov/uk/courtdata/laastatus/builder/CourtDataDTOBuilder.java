88acdc2f439b0bc96f6cefb068f6b594c8796ca7
liam.murphy <liam.murphy@digital.justice.gov.uk>
2025-08-13 14:56:15 +0100

    LASB-4372 - Addressed code comments

--- maat-court-data-api/src/main/java/gov/uk/courtdata/laastatus/builder/CourtDataDTOBuilder.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/laastatus/builder/CourtDataDTOBuilder.java
@@ -55,7 +55,7 @@ public class CourtDataDTOBuilder {
             offence.setOffenceId(offenceEntity.map(OffenceEntity::getOffenceId).orElse(null));
 
             log.info("legal status {}, date {}", offence.getLegalAidStatus(), offence.getLegalAidStatusDate());
-            if (!offence.getLegalAidStatus().equals("AP") && offence.getLegalAidStatusDate() != null) {
+            if (offence.getLegalAidStatusDate() != null && !offence.getLegalAidStatus().equals("AP")) {
                 log.info("Offence legal status date {}", offenceEntity.get().getLegalAidStatusDate());
                 offence.setLegalAidStatusDate(offenceEntity.get().getLegalAidStatusDate().toString());
             }

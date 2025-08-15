bb7956e14c2c1031efd9523b932dc9910c428a89
liam.murphy <liam.murphy@digital.justice.gov.uk>
2025-08-15 13:17:12 +0100

    LASB-4373 - fixed and improved performance of Offence native queries

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/OffenceRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/OffenceRepository.java
@@ -23,14 +23,14 @@ public interface OffenceRepository extends JpaRepository<OffenceEntity, AsnSeqTx
     @Query(value = "SELECT COUNT(*) FROM MLA.XXMLA_OFFENCE WHERE CASE_ID = ?1 AND ASN_SEQ = ?2", nativeQuery = true)
     Integer getOffenceCountForAsnSeq(Integer caseId, String asnSeq);
 
-    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE ROWNUM = 1 AND CASE_ID = :caseId AND ASN_SEQ = :asnSeq " +
-                    "AND OFFENCE_CODE = :offenceCode ORDER BY TX_ID DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE CASE_ID = :caseId AND ASN_SEQ = :asnSeq " +
+                    "AND OFFENCE_CODE = :offenceCode ORDER BY TX_ID DESC FETCH FIRST 1 ROW ONLY", nativeQuery = true)
     Optional<OffenceEntity> findByMaxTxId(Integer caseId, String offenceCode, String asnSeq);
 
     List<OffenceEntity> findByCaseId(Integer caseId);
 
-    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE ROWNUM = 1 AND CASE_ID = :caseId AND APPLICATION_FLAG = :applicationFlag " +
-                    "AND OFFENCE_ID = :offenceId ORDER BY TX_ID DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE CASE_ID = :caseId AND APPLICATION_FLAG = :applicationFlag " +
+                    "AND OFFENCE_ID = :offenceId ORDER BY TX_ID DESC FETCH FIRST 1 ROW ONLY", nativeQuery = true)
     Optional<OffenceEntity> findApplicationByOffenceCode(Integer caseId, String offenceId, Integer applicationFlag);
 
     @Query(value = "SELECT COUNT(*) FROM MLA.XXMLA_OFFENCE WHERE CASE_ID = ?1 AND OFFENCE_ID = ?2 AND CC_NEW_OFFENCE = 'Y' AND APPLICATION_FLAG = 0", nativeQuery = true)

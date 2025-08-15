d7618e97643c0e29789449bfaae99178c4a94f35
liam.murphy <liam.murphy@digital.justice.gov.uk>
2025-08-15 13:36:11 +0100

    LASB-4373 - fixed and improved performance of Offence native queries

--- maat-court-data-api/src/main/java/gov/uk/courtdata/repository/OffenceRepository.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/repository/OffenceRepository.java
@@ -30,7 +30,7 @@ public interface OffenceRepository extends JpaRepository<OffenceEntity, AsnSeqTx
     List<OffenceEntity> findByCaseId(Integer caseId);
 
     @Query(value = "SELECT * FROM MLA.XXMLA_OFFENCE WHERE ROWNUM = 1 AND CASE_ID = :caseId AND APPLICATION_FLAG = :applicationFlag " +
-            "AND OFFENCE_ID = :offenceId ORDER BY TX_ID DESC", nativeQuery = true)
+                    "AND OFFENCE_ID = :offenceId ORDER BY TX_ID DESC", nativeQuery = true)
     Optional<OffenceEntity> findApplicationByOffenceCode(Integer caseId, String offenceId, Integer applicationFlag);
 
     @Query(value = "SELECT COUNT(*) FROM MLA.XXMLA_OFFENCE WHERE CASE_ID = ?1 AND OFFENCE_ID = ?2 AND CC_NEW_OFFENCE = 'Y' AND APPLICATION_FLAG = 0", nativeQuery = true)

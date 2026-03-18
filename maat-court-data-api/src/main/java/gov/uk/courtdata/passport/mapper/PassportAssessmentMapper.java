b4a721e04f72e6ee8b3adccf0932f2a4c8d74f85
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2026-03-18 15:49:42 +0000

    refactor: amendments to find passport response mapping following commons update

--- maat-court-data-api/src/main/java/gov/uk/courtdata/passport/mapper/PassportAssessmentMapper.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/passport/mapper/PassportAssessmentMapper.java
@@ -1,6 +1,8 @@
 package gov.uk.courtdata.passport.mapper;
 
 import gov.uk.courtdata.entity.PassportAssessmentEntity;
+import java.util.Map;
+import java.util.Set;
 import org.mapstruct.Condition;
 import org.mapstruct.ConditionStrategy;
 import org.mapstruct.Mapper;
@@ -15,25 +17,26 @@ import uk.gov.justice.laa.crime.enums.PassportAssessmentDecisionReason;
     implementationName = "<CLASS_NAME>V2Impl", uses = PassportAssessmentMapperHelper.class)
 public interface PassportAssessmentMapper {
 
+    @Mapping(target = "caseManagementUnitId", source = "cmuId")
     @Mapping(target = "legacyAssessmentId", source = "id")
     @Mapping(target = "assessmentReason", source = "nworCode")
     @Mapping(target = "reviewType", source = "rtCode")
-    @Mapping(target = "declaredUnder18", source = "passportAssessmentEntity", 
+    @Mapping(target = "declaredUnder18", source = "passportAssessmentEntity",
         qualifiedByName = "under18Mapper")
     @Mapping(target = "declaredBenefit", source = "passportAssessmentEntity",
         qualifiedByName = "declaredBenefitMapper", conditionQualifiedByName = "isOver18")
-    @Mapping(target = "assessmentDecision", source = "passportAssessmentEntity", 
+    @Mapping(target = "assessmentDecision", source = "passportAssessmentEntity",
         qualifiedByName = "assessmentDecisionMapper")
-    @Mapping(target = "decisionReason", source = "passportAssessmentEntity", 
+    @Mapping(target = "decisionReason", source = "passportAssessmentEntity",
         qualifiedByName = "decisionReasonMapper")
     ApiGetPassportedAssessmentResponse toApiGetPassportedAssessmentResponse(
         PassportAssessmentEntity passportAssessmentEntity);
-    
+
     @Named("under18Mapper")
     default boolean mapUnder18(PassportAssessmentEntity passportAssessmentEntity) {
         return ((passportAssessmentEntity.getUnder18HeardInYouthCourt() != null
             && passportAssessmentEntity.getUnder18HeardInYouthCourt().equals("Y"))
-            || (passportAssessmentEntity.getUnder18HeardInMagsCourt() != null 
+            || (passportAssessmentEntity.getUnder18HeardInMagsCourt() != null
             && passportAssessmentEntity.getUnder18HeardInMagsCourt().equals("Y")));
     }
 
@@ -44,40 +47,35 @@ public interface PassportAssessmentMapper {
     }
 
     @Named("assessmentDecisionMapper")
-    default PassportAssessmentDecision mapAssessmentDecision(PassportAssessmentEntity passportAssessmentEntity) {
-        switch (passportAssessmentEntity.getResult()) {
-            case "PASS":
-                return PassportAssessmentDecision.PASS;
-            case "TEMP":
-                return PassportAssessmentDecision.TEMP_PASS;
-            case "FAIL CONTINUE":
-                return PassportAssessmentDecision.FAIL_BYPASS;
-        }
-
-        return null;
+    default PassportAssessmentDecision mapAssessmentDecision(
+        PassportAssessmentEntity passportAssessmentEntity) {
+        return PassportAssessmentDecision.getFrom(passportAssessmentEntity.getResult());
     }
-    
+
     @Named("decisionReasonMapper")
-    default PassportAssessmentDecisionReason mapDecisionReason(PassportAssessmentEntity passportAssessmentEntity) {
-        if (passportAssessmentEntity.getResult().equals("PASS")) {
-            switch (passportAssessmentEntity.getPcobConfirmation()) {
-                case "AGEREL":
-                    return PassportAssessmentDecisionReason.APPLICANT_AGE;
-                case "DWP":
-                    return PassportAssessmentDecisionReason.DWP_CHECK;
-                case "DOCSUP":
-                    return PassportAssessmentDecisionReason.DOCUMENTATION_SUPPLIED;
-            }  
-        } else if (passportAssessmentEntity.getResult().equals("TEMP")) {
-            if (passportAssessmentEntity.getPcobConfirmation().equals("NOCONFPOS")) {
-                return PassportAssessmentDecisionReason.DWP_CHECK_UNAVAILABLE;
-            } else if (passportAssessmentEntity.getPcobConfirmation().equals("INCUSTODY")) {
-                return PassportAssessmentDecisionReason.IN_CUSTODY;
-            }
-        } else if (passportAssessmentEntity.getResult().equals("FAIL CONTINUE")) {
-            return PassportAssessmentDecisionReason.DWP_CHECK;
+    default PassportAssessmentDecisionReason mapDecisionReason(
+        PassportAssessmentEntity passportAssessmentEntity) {
+        final Map<PassportAssessmentDecision, Set<PassportAssessmentDecisionReason>> decisionReasonCombinations = Map.of(
+            PassportAssessmentDecision.PASS, Set.of(PassportAssessmentDecisionReason.APPLICANT_AGE,
+                PassportAssessmentDecisionReason.DWP_CHECK,
+                PassportAssessmentDecisionReason.DOCUMENTATION_SUPPLIED),
+            PassportAssessmentDecision.TEMP_PASS,
+            Set.of(PassportAssessmentDecisionReason.DWP_CHECK_UNAVAILABLE,
+                PassportAssessmentDecisionReason.IN_CUSTODY),
+            PassportAssessmentDecision.FAIL_BYPASS,
+            Set.of(PassportAssessmentDecisionReason.DWP_CHECK),
+            PassportAssessmentDecision.FAIL, Set.of()
+        );
+
+        String pcobConfirmation = passportAssessmentEntity.getPcobConfirmation();
+        Set<PassportAssessmentDecisionReason> allowedReasons = decisionReasonCombinations.get(
+            PassportAssessmentDecision.getFrom(passportAssessmentEntity.getResult()));
+
+        if (allowedReasons != null && (allowedReasons.isEmpty() || allowedReasons.contains(
+            PassportAssessmentDecisionReason.getFrom(pcobConfirmation)))) {
+            return PassportAssessmentDecisionReason.getFrom(pcobConfirmation);
+        } else {
+            return null;
         }
-        
-        return null;
     }
 }

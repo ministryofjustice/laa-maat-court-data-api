c77faa3c82dc76b3df35b9aac020f65ef3d5f9d8
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2026-05-21 14:40:53 +0100

    [LCAM-2079] Remove unused Passport Assessment V1 endpoints
    Also removed associated code/tests. The Passport Assessor Details endpoint appears to be in use by the Application Tracking Service, so have left that in place.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/service/PassportAssessmentService.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/service/PassportAssessmentService.java
@@ -1,91 +1,20 @@
 package gov.uk.courtdata.assessment.service;
 
 import gov.uk.courtdata.assessment.impl.PassportAssessmentImpl;
-import gov.uk.courtdata.assessment.mapper.PassportAssessmentMapper;
 import gov.uk.courtdata.dto.AssessorDetails;
-import gov.uk.courtdata.dto.PassportAssessmentDTO;
 import gov.uk.courtdata.entity.PassportAssessmentEntity;
 import gov.uk.courtdata.exception.RequestedObjectNotFoundException;
-import gov.uk.courtdata.exception.ValidationException;
-import gov.uk.courtdata.helper.ReflectionHelper;
-import gov.uk.courtdata.model.assessment.CreatePassportAssessment;
-import gov.uk.courtdata.model.assessment.UpdatePassportAssessment;
-import gov.uk.courtdata.repository.PassportAssessmentRepository;
 import gov.uk.courtdata.util.UserEntityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PassportAssessmentService {
-    public static final String STATUS_COMPLETE = "COMPLETE";
     private final PassportAssessmentImpl passportAssessmentImpl;
-    private final PassportAssessmentMapper passportAssessmentMapper;
-    private final PassportAssessmentRepository passportAssessmentRepository;
-    private final AssessmentReplacementService assessmentReplacementService;
-
-    @Transactional(readOnly = true)
-    public PassportAssessmentDTO find(Integer passportAssessmentId) {
-        PassportAssessmentEntity passportAssessmentEntity = passportAssessmentImpl.find(passportAssessmentId);
-        if (passportAssessmentEntity == null) {
-            throw new RequestedObjectNotFoundException(
-                    String.format("No Passport Assessment found for ID: %s", passportAssessmentId));
-        }
-        return buildPassportAssessmentDTO(passportAssessmentEntity);
-    }
-
-    @Transactional(readOnly = true)
-    public PassportAssessmentDTO findByRepId(int repId) {
-        PassportAssessmentEntity passportAssessmentEntity = passportAssessmentImpl.findByRepId(repId);
-        if (passportAssessmentEntity == null) {
-            throw new RequestedObjectNotFoundException(
-                    String.format("No Passport Assessment found for REP ID: %s", repId));
-        }
-        return buildPassportAssessmentDTO(passportAssessmentEntity);
-    }
-
-    @Transactional
-    public PassportAssessmentDTO update(UpdatePassportAssessment updatePassportAssessment) {
-        log.info("Update Passport Assessment - Transaction Processing - Start");
-        PassportAssessmentDTO passportAssessmentDTO =
-                passportAssessmentMapper.updatePassportAssessmentToPassportAssessmentDTO(updatePassportAssessment);
-        log.info("Updating existing passport assessment record");
-        PassportAssessmentEntity existingPassportAssessmentEntity =
-                passportAssessmentImpl.find(passportAssessmentDTO.getId());
-        if (existingPassportAssessmentEntity.getPastStatus().equals(STATUS_COMPLETE)) {
-            throw new ValidationException("User cannot modify a completed assessment");
-        }
-        PassportAssessmentEntity passportAssessmentEntity = passportAssessmentImpl.update(passportAssessmentDTO);
-        log.info("Update Passport Assessment - Transaction Processing - End");
-        return buildPassportAssessmentDTO(passportAssessmentEntity);
-    }
-
-    public void delete(Integer passportAssessmentId) {
-        passportAssessmentImpl.delete(passportAssessmentId);
-    }
-
-    @Transactional
-    public PassportAssessmentDTO create(CreatePassportAssessment createPassportAssessment) {
-        log.info("Create Passport Assessment - Transaction Processing - Start");
-        PassportAssessmentDTO passportAssessmentDTO =
-                passportAssessmentMapper.createPassportAssessmentToPassportAssessmentDTO(createPassportAssessment);
-        log.info("Creating new passport assessment record");
-        PassportAssessmentEntity assessmentEntity = passportAssessmentImpl.create(passportAssessmentDTO);
-        log.info("Setting outdated records as replaced");
-        assessmentReplacementService.replacePreviousAssessments(assessmentEntity);
-        log.info("Create Passport Assessment - Transaction Processing - End");
-        return buildPassportAssessmentDTO(assessmentEntity);
-    }
-
-    PassportAssessmentDTO buildPassportAssessmentDTO(PassportAssessmentEntity assessmentEntity) {
-        return passportAssessmentMapper.passportAssessmentEntityToPassportAssessmentDTO(assessmentEntity);
-    }
 
     public AssessorDetails findPassportAssessorDetails(int passportAssessmentId) {
         PassportAssessmentEntity passportAssessmentEntity = passportAssessmentImpl.find(passportAssessmentId);
@@ -99,15 +28,5 @@ public class PassportAssessmentService {
                 .fullName(UserEntityUtils.extractFullName(passportAssessmentEntity.getUserCreatedEntity()))
                 .userName(passportAssessmentEntity.getUserCreated())
                 .build();
-    }
-
-    public void patch(int passportAssessmentId, Map<String, Object> updateFields) {
-        PassportAssessmentEntity passportAssessmentEntity = passportAssessmentRepository
-                .findById(passportAssessmentId)
-                .orElseThrow(() -> new RequestedObjectNotFoundException(String.format(
-                        "No Passport Assessment found for passport assessment Id: [%s]", passportAssessmentId)));
-
-        ReflectionHelper.updateEntityFromMap(passportAssessmentEntity, updateFields);
-        passportAssessmentRepository.save(passportAssessmentEntity);
     }
 }

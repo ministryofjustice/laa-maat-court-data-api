8e7924091606704b861062f3fea78c58377b5e17
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2026-05-21 14:40:53 +0100

    [LCAM-2079] Remove unused Passport Assessment V1 endpoints
    Also removed associated code/tests. The Passport Assessor Details endpoint appears to be in use by the Application Tracking Service, so have left that in place.

--- maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/controller/PassportAssessmentController.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/controller/PassportAssessmentController.java
@@ -1,35 +1,19 @@
 package gov.uk.courtdata.assessment.controller;
 
-import gov.uk.courtdata.annotation.NotFoundApiResponse;
 import gov.uk.courtdata.annotation.StandardApiResponse;
 import gov.uk.courtdata.assessment.service.PassportAssessmentService;
-import gov.uk.courtdata.assessment.validator.PassportAssessmentValidationProcessor;
-import gov.uk.courtdata.controller.StandardApiResponseCodes;
 import gov.uk.courtdata.dto.AssessorDetails;
-import gov.uk.courtdata.dto.PassportAssessmentDTO;
-import gov.uk.courtdata.enums.LoggingData;
-import gov.uk.courtdata.model.assessment.CreatePassportAssessment;
-import gov.uk.courtdata.model.assessment.UpdatePassportAssessment;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,106 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PassportAssessmentController {
 
     private final PassportAssessmentService passportAssessmentService;
-    private final PassportAssessmentValidationProcessor passportAssessmentValidationProcessor;
-
-    @GetMapping(value = "/{passportAssessmentId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(description = "Retrieve a passport assessment record")
-    @ApiResponse(
-            responseCode = "200",
-            content =
-                    @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = PassportAssessmentDTO.class)))
-    @StandardApiResponse
-    @NotFoundApiResponse
-    public ResponseEntity<PassportAssessmentDTO> getAssessment(
-            @PathVariable int passportAssessmentId,
-            @Parameter(description = "Used for tracing calls")
-                    @RequestHeader(value = "Laa-Transaction-Id", required = false)
-                    String laaTransactionId) {
-        log.debug("Get Passport Assessment Request Received");
-        passportAssessmentValidationProcessor.validate(passportAssessmentId);
-        PassportAssessmentDTO passportAssessment = passportAssessmentService.find(passportAssessmentId);
-        return ResponseEntity.ok(passportAssessment);
-    }
-
-    @GetMapping(value = "repId/{repId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(description = "Retrieve a passport assessment record by repId")
-    @ApiResponse(
-            responseCode = "200",
-            content =
-                    @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = PassportAssessmentDTO.class)))
-    @NotFoundApiResponse
-    @StandardApiResponse
-    public ResponseEntity<PassportAssessmentDTO> getAssessmentByRepId(
-            @PathVariable int repId,
-            @Parameter(description = "Used for tracing calls")
-                    @RequestHeader(value = "Laa-Transaction-Id", required = false)
-                    String laaTransactionId) {
-        LoggingData.MAAT_ID.putInMDC(repId);
-        log.info("Get Passport Assessment by repId = {}", repId);
-        PassportAssessmentDTO passportAssessment = passportAssessmentService.findByRepId(repId);
-        return ResponseEntity.ok(passportAssessment);
-    }
-
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(description = "Update a passport assessment record")
-    @ApiResponse(
-            responseCode = "200",
-            content =
-                    @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = PassportAssessmentDTO.class)))
-    @StandardApiResponse
-    public ResponseEntity<PassportAssessmentDTO> updateAssessment(
-            @Parameter(
-                            description = "Passport assessment data",
-                            content =
-                                    @Content(
-                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                            schema = @Schema(implementation = UpdatePassportAssessment.class)))
-                    @RequestBody
-                    UpdatePassportAssessment passportAssessment,
-            @Parameter(description = "Used for tracing calls")
-                    @RequestHeader(value = "Laa-Transaction-Id", required = false)
-                    String laaTransactionId) {
-        LoggingData.MAAT_ID.putInMDC(passportAssessment.getRepId());
-        log.debug("Update Passport Assessment Request Received");
-        passportAssessmentValidationProcessor.validate(passportAssessment);
-        PassportAssessmentDTO updatedAssessment = passportAssessmentService.update(passportAssessment);
-        return ResponseEntity.ok(updatedAssessment);
-    }
-
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(description = "Create a new passport assessment record")
-    @ApiResponse(
-            responseCode = "200",
-            content =
-                    @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = PassportAssessmentDTO.class)))
-    @StandardApiResponse
-    public ResponseEntity<PassportAssessmentDTO> createAssessment(
-            @Parameter(
-                            description = "Passport assessment data",
-                            content =
-                                    @Content(
-                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                            schema = @Schema(implementation = CreatePassportAssessment.class)))
-                    @RequestBody
-                    CreatePassportAssessment passportAssessment,
-            @Parameter(description = "Used for tracing calls")
-                    @RequestHeader(value = "Laa-Transaction-Id", required = false)
-                    String laaTransactionId) {
-        LoggingData.USN.putInMDC(passportAssessment.getUsn());
-        LoggingData.MAAT_ID.putInMDC(passportAssessment.getRepId());
-        log.debug("Create Passport Assessment Request Received");
-        passportAssessmentValidationProcessor.validate(passportAssessment);
-        PassportAssessmentDTO newAssessment = passportAssessmentService.create(passportAssessment);
-        return ResponseEntity.ok(newAssessment);
-    }
 
     @GetMapping(
             value = "/{passportAssessmentId}/passport-assessor-details",
@@ -151,15 +35,5 @@ public class PassportAssessmentController {
     public ResponseEntity<AssessorDetails> findPassportAssessorDetails(@PathVariable int passportAssessmentId) {
         AssessorDetails assessorDetails = passportAssessmentService.findPassportAssessorDetails(passportAssessmentId);
         return ResponseEntity.ok(assessorDetails);
-    }
-
-    @PatchMapping(value = "/{passportAssessmentId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(description = "Update a subset of passport assessment")
-    @StandardApiResponseCodes
-    public ResponseEntity<Void> patchPassportAssessment(
-            @PathVariable int passportAssessmentId, @RequestBody Map<String, Object> updateFields) {
-        log.info("Request received to update Passport Assessment with Id: {}", passportAssessmentId);
-        passportAssessmentService.patch(passportAssessmentId, updateFields);
-        return ResponseEntity.ok().build();
     }
 }
--- maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/impl/PassportAssessmentImpl.java
+++ maat-court-data-api/src/main/java/gov/uk/courtdata/assessment/impl/PassportAssessmentImpl.java
@@ -1,13 +1,9 @@
 package gov.uk.courtdata.assessment.impl;
 
-import gov.uk.courtdata.assessment.mapper.PassportAssessmentMapper;
-import gov.uk.courtdata.dto.PassportAssessmentDTO;
 import gov.uk.courtdata.entity.PassportAssessmentEntity;
 import gov.uk.courtdata.repository.PassportAssessmentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 
@@ -16,60 +12,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PassportAssessmentImpl {
 
-    private final PassportAssessmentMapper assessmentMapper;
     private final PassportAssessmentRepository passportAssessmentRepository;
 
     public PassportAssessmentEntity find(Integer passportAssessmentId) {
         return passportAssessmentRepository.getReferenceById(passportAssessmentId);
-    }
-
-    public PassportAssessmentEntity findByRepId(int repId) {
-        return passportAssessmentRepository.findByRepId(repId).orElse(null);
-    }
-
-    public PassportAssessmentEntity update(PassportAssessmentDTO passportAssessmentDTO) {
-        PassportAssessmentEntity existingPassportAssessment =
-                passportAssessmentRepository.getReferenceById(passportAssessmentDTO.getId());
-        existingPassportAssessment.setNworCode(passportAssessmentDTO.getNworCode());
-        existingPassportAssessment.setCmuId(passportAssessmentDTO.getCmuId());
-        existingPassportAssessment.setAssessmentDate(passportAssessmentDTO.getAssessmentDate());
-        existingPassportAssessment.setPartnerBenefitClaimed(passportAssessmentDTO.getPartnerBenefitClaimed());
-        existingPassportAssessment.setPartnerFirstName(passportAssessmentDTO.getPartnerFirstName());
-        existingPassportAssessment.setPartnerSurname(passportAssessmentDTO.getPartnerSurname());
-        existingPassportAssessment.setPartnerNiNumber(passportAssessmentDTO.getPartnerNiNumber());
-        existingPassportAssessment.setPartnerDob(passportAssessmentDTO.getPartnerDob());
-        existingPassportAssessment.setIncomeSupport(passportAssessmentDTO.getIncomeSupport());
-        existingPassportAssessment.setJobSeekers(passportAssessmentDTO.getJobSeekers());
-        existingPassportAssessment.setStatePensionCredit(passportAssessmentDTO.getStatePensionCredit());
-        existingPassportAssessment.setPcobConfirmation(passportAssessmentDTO.getPcobConfirmation());
-        existingPassportAssessment.setResult(passportAssessmentDTO.getResult());
-        existingPassportAssessment.setDwpResult(passportAssessmentDTO.getDwpResult());
-        existingPassportAssessment.setPassportNote(passportAssessmentDTO.getPassportNote());
-        existingPassportAssessment.setUnder18HeardInYouthCourt(passportAssessmentDTO.getUnder18HeardInYouthCourt());
-        existingPassportAssessment.setUnder18HeardInMagsCourt(passportAssessmentDTO.getUnder18HeardInMagsCourt());
-        existingPassportAssessment.setLastSignOnDate(passportAssessmentDTO.getLastSignOnDate());
-        existingPassportAssessment.setEsa(passportAssessmentDTO.getEsa());
-        existingPassportAssessment.setUnder18FullEducation(passportAssessmentDTO.getUnder18FullEducation());
-        existingPassportAssessment.setUnder16(passportAssessmentDTO.getUnder16());
-        existingPassportAssessment.setBetween16And17(passportAssessmentDTO.getBetween16And17());
-        existingPassportAssessment.setPastStatus(passportAssessmentDTO.getPastStatus());
-        existingPassportAssessment.setWhoDWPChecked(passportAssessmentDTO.getWhoDWPChecked());
-        existingPassportAssessment.setDateCompleted(passportAssessmentDTO.getDateCompleted());
-        existingPassportAssessment.setUserModified(passportAssessmentDTO.getUserModified());
-        existingPassportAssessment.setDateModified(
-                passportAssessmentDTO.getDateModified() != null
-                        ? passportAssessmentDTO.getDateModified()
-                        : LocalDateTime.now());
-        return passportAssessmentRepository.save(existingPassportAssessment);
-    }
-
-    public void delete(Integer passportAssessmentId) {
-        passportAssessmentRepository.deleteById(passportAssessmentId);
-    }
-
-    public PassportAssessmentEntity create(PassportAssessmentDTO passportAssessmentDTO) {
-        PassportAssessmentEntity passportAssessmentEntity =
-                assessmentMapper.passportAssessmentDtoToPassportAssessmentEntity(passportAssessmentDTO);
-        return passportAssessmentRepository.save(passportAssessmentEntity);
     }
 }

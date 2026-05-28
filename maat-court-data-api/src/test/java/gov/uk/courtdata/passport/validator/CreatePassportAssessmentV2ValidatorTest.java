f600443f76cdd6e9b74cc9ad2051667c1613ee2e
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2026-05-28 11:20:08 +0100

    chore: applying spotless.

--- maat-court-data-api/src/test/java/gov/uk/courtdata/passport/validator/CreatePassportAssessmentV2ValidatorTest.java
+++ maat-court-data-api/src/test/java/gov/uk/courtdata/passport/validator/CreatePassportAssessmentV2ValidatorTest.java
@@ -155,8 +155,8 @@ class CreatePassportAssessmentV2ValidatorTest {
     }
 
     @Test
-    void givenOutstandingAssessment_whenValidateIsInvoked_outstandingAssessmentErrorShouldPresent(){
-        ErrorMessage expectedError = new ErrorMessage("test","test error should be present.");
+    void givenOutstandingAssessment_whenValidateIsInvoked_outstandingAssessmentErrorShouldPresent() {
+        ErrorMessage expectedError = new ErrorMessage("test", "test error should be present.");
         when(repOrderService.exists(REP_ID)).thenReturn(true);
         when(partnerResolver.hasLinkedPartner(REP_ID, APPLICANT_ID)).thenReturn(true);
         when(outstandingAssessmentService.checkForOutstandingAssessments(any())).thenReturn(Optional.of(expectedError));
